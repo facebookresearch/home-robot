41e27e78efd980b2db6a49a55200ba1b0286067a
theophilee <theophile.gervet@gmail.com>
2022-11-10 08:51:02 -0500

    Try VLMaps

---
+++ src/agent/perception/segmentation/lseg/__init__.py
@@ -0,0 +1,25 @@
+import torch
+
+from modules.models.lseg_net import LSegEncNet
+
+
+def load_lseg_model_for_inference(lang: str,
+                                  checkpoint_path: str,
+                                  device: torch.device,
+                                  crop_size: int = 480) -> LSegEncNet:
+    model = LSegEncNet(lang,
+                       arch_option=0,
+                       block_depth=0,
+                       activation='lrelu',
+                       crop_size=crop_size)
+    model_state_dict = model.state_dict()
+
+    pretrained_state_dict = torch.load(checkpoint_path)
+    pretrained_state_dict = {k.lstrip('net.'): v
+                             for k, v in pretrained_state_dict['state_dict'].items()}
+    model_state_dict.update(pretrained_state_dict)
+    model.load_state_dict(pretrained_state_dict)
+
+    model.eval()
+    model = model.to(device)
+    return model
