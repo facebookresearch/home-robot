056cd5f218c05747a908160975db3be1fba33b77
theophilee <theophile.gervet@gmail.com>
2022-11-11 08:53:50 -0500

    Try VLMaps

--- src/agent/mapping/metric/semantic/vision_language_2d_semantic_map_module.py
+++ src/agent/mapping/metric/semantic/vision_language_2d_semantic_map_module.py
@@ -369,14 +369,14 @@ class VisionLanguage2DSemanticMapModule(nn.Module):
         for e in range(batch_size):
             update_mask = translated[e, 4:].sum(0) > 0
             print("update_mask.shape", update_mask.shape)
-            print("prev_map[:, :4][update_mask].shape", prev_map[:, :4][update_mask].shape)
-            print("prev_map[:, :4, update_mask].shape", prev_map[:, :4, update_mask].shape)
-            print("translated[:, :4, update_mask].shape", translated[:, :4, update_mask].shape)
+            print("prev_map[e, 4:][update_mask].shape", prev_map[e, 4:][update_mask].shape)
+            print("prev_map[e, 4:, update_mask].shape", prev_map[e, 4:, update_mask].shape)
+            print("translated[e, 4:, update_mask].shape", translated[e, 4:, update_mask].shape)
             current_map[e, 4:, update_mask] = torch.mean(
                 torch.cat(
                     (
-                        prev_map[:, :4, update_mask].unsqueeze(1),
-                        translated[:, :4, update_mask].unsqueeze(1)
+                        prev_map[e, 4:, update_mask].unsqueeze(1),
+                        translated[e, 4:, update_mask].unsqueeze(1)
                     ),
                     1
                 ),
