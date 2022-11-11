0c1c04925045aca213d00409a9e0e3a6f7cf2ccb
theophilee <theophile.gervet@gmail.com>
2022-11-10 20:50:29 -0500

    Try VLMaps

--- src/agent/mapping/metric/semantic/vision_language_2d_semantic_map_module.py
+++ src/agent/mapping/metric/semantic/vision_language_2d_semantic_map_module.py
@@ -276,6 +276,7 @@ class VisionLanguage2DSemanticMapModule(nn.Module):
         )
 
         # TODO Batch LSeg inference across time
+        print(rgb.shape)
         pixel_features = self.lseg.encode(rgb)
 
         feat[:, 1:, :] = nn.AvgPool2d(self.du_scale)(pixel_features).view(
