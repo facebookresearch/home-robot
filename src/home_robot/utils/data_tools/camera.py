c5770e1119b224612dcb68393d58127e9b1068a8
Chris Paxton <cpaxton@fb.com>
2022-12-09 14:37:09 -0500

    remove dead or unused or unfinished code and do some cleanup

--- src/home_robot/utils/data_tools/camera.py
+++ src/home_robot/utils/data_tools/camera.py
@@ -15,6 +15,9 @@ import pickle
 
 
 class Camera(object):
+    """Camera object storing information about a single camera.
+    Includes projection and pose information."""
+
     def __init__(
         self,
         pos=None,
@@ -72,87 +75,3 @@ class Camera(object):
 
     def get_pose(self):
         return self.pose_matrix.copy()
-
-
-class Observation(object):
-    """A particular observation along a trajectory"""
-
-    def __init__(self, cam, frame):
-        raise NotImplementedError()
-
-
-class CalibratedScene(object):
-    """
-    Stores the information related to a single calibrated camera. Create by pointing to a
-    directory which will contain all of this data.
-
-    This provides a wrapper which loads image data and does other things like that.
-
-    It also lets us visualize stuff.
-    """
-
-    caldata = "caldata.pkl"
-    calibration_json = "calibration.json"
-    calibration_points = "calibration_points.json"
-
-    def get_camera_frames(self):
-        geometries = []
-        for camera in self.cameras:
-            geom = o3d.geometry.TriangleMesh.create_coordinate_frame(
-                size=0.1, origin=camera.pos
-            )
-            geom.rotate(camera.rot, center=camera.pos)
-            geometries.append(geom)
-        return geometries
-
-    def show_camera_poses(self):
-        geometries = self.get_camera_frames()
-        o3d.visualization.draw_geometries(geometries)
-
-    def load_cameras_from_json(self, json_filename, pkl_filename=None):
-        """create cameras from a config json file"""
-        cameras = []
-        with open(json_filename, "r") as f:
-            data = json.load(f)
-        with open(pkl_filename, "rb") as f:
-            caldata = pickle.load(f)
-
-        for i in range(len(data)):
-            camera_rot = np.array(data[i]["camera_base_ori"])
-            camera_pos = data[i]["camera_base_pos"]
-            pixel_err = data[i]["pixel_error"]
-            cameras.append(Camera(camera_pos, camera_rot, pixel_err))
-        return cameras
-
-    def __init__(self, dirname, load_from_pkl=True):
-        """
-        Load calibration data from different sources
-        """
-        self.cameras = []
-        self.add(dirname, load_from_pkl)
-
-    def load_cameras_from_pkl(self, pkl_filename):
-        with open(pkl_filename, "rb") as f:
-            caldata = pickle.load(f)
-        import pdb
-
-        pdb.set_trace()
-
-    # def add(self, dirname, load_from_pkl):
-    def add(self, filename):
-        # calibration_filename = os.path.join(dirname, self.calibration_json)
-        # calibration_pkl = os.path.join(dirname, self.caldata)
-        self.cameras += self.load_cameras_from_pkl(filename)
-        # self.load_cameras_from_json(calibration_filename, calibration_pkl)
-
-
-if __name__ == "__main__":
-    root = "~/data/calibratedExample0426"
-    root = os.path.expanduser(root)
-    print(root)
-    world_cam_dir = os.path.join(root, "WorldCam")
-    ee_cam_dir = os.path.join(root, "EECam")
-    scene = CalibratedScene(world_cam_dir)
-    # scene.add(ee_cam_dir)
-    # Debugging - show the camera poses
-    scene.show_camera_poses()
--- src/home_robot/utils/data_tools/image.py
+++ src/home_robot/utils/data_tools/image.py
@@ -8,7 +8,10 @@ from pygifsicle import optimize
 
 
 def img_from_bytes(data: bytes, height=None, width=None) -> np.ndarray:
+    """Convert image from png bytes"""
     image = Image.open(io.BytesIO(data), mode="r", formats=["png"])
+    # TODO: decide if default image format should switch over to webp
+    # Issue: not quite as good at handling depth
     # image = Image.open(data, mode='r', formats=['webp'])
     if height and width:
         image = image.resize([width, height])
@@ -16,16 +19,9 @@ def img_from_bytes(data: bytes, height=None, width=None) -> np.ndarray:
 
 
 def pil_to_bytes(img: Image) -> bytes:
+    """Convert image to bytes using PIL"""
     data = io.BytesIO()
-    # img.save(data, format="webp", lossless=True)
     img.save(data, format="png")
-    # data = data.getvalue()
-    # img = Image.open(data).convert('RGB')
-    # img = img_from_bytes(data.getvalue())
-    # import matplotlib.pyplot as plt
-    # plt.figure(1)
-    # plt.imshow(img)
-    # plt.show()
     return data.getvalue()
 
 
@@ -58,7 +54,6 @@ def png_to_gif(
     ):
         bindata = img_stream[k][()]
         img = img_from_bytes(bindata, height, width)
-        # img = img_from_h5(img, i)
         gif.append(img)
     if save:
         imageio.mimsave(name, gif)
@@ -103,69 +98,6 @@ def schema_to_gifs(filename: str):
         optimize(gif_name)
 
 
-##### RGB Augmentations #####
-
-
-def standardize_image(image):
-    """Convert a numpy.ndarray [H x W x 3] of images to [0,1] range, and then standardizes
-    @return: a [H x W x 3] numpy array of np.float32
-    """
-    image_standardized = np.zeros_like(image).astype(np.float32)
-
-    mean = [0.485, 0.456, 0.406]
-    std = [0.229, 0.224, 0.225]
-    for i in range(3):
-        image_standardized[..., i] = (image[..., i] / 255.0 - mean[i]) / std[i]
-
-    return image_standardized
-
-
-def unstandardize_image(image):
-    """Convert a numpy.ndarray [H x W x 3] standardized image back to RGB (type np.uint8)
-    Inverse of standardize_image()
-    @return: a [H x W x 3] numpy array of type np.uint8
-    """
-
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    orig_img = (image * std[None, None, :] + mean[None, None, :]) * 255.0
-    return orig_img.round().astype(np.uint8)
-
-
-def random_color_warp(image, d_h=None, d_s=None, d_l=None):
-    """Given an RGB image [H x W x 3], add random hue, saturation and luminosity to the image
-    Code adapted from: https://github.com/yuxng/PoseCNN/blob/master/lib/utils/blob.py
-    """
-    H, W, _ = image.shape
-
-    image_color_warped = np.zeros_like(image)
-
-    # Set random hue, luminosity and saturation which ranges from -0.1 to 0.1
-    if d_h is None:
-        d_h = (random.random() - 0.5) * 0.2 * 256
-    if d_l is None:
-        d_l = (random.random() - 0.5) * 0.2 * 256
-    if d_s is None:
-        d_s = (random.random() - 0.5) * 0.2 * 256
-
-    # Convert the RGB to HLS
-    hls = cv2.cvtColor(image.round().astype(np.uint8), cv2.COLOR_RGB2HLS)
-    h, l, s = cv2.split(hls)
-
-    # Add the values to the image H, L, S
-    new_h = (np.round((h + d_h)) % 256).astype(np.uint8)
-    new_l = np.round(np.clip(l + d_l, 0, 255)).astype(np.uint8)
-    new_s = np.round(np.clip(s + d_s, 0, 255)).astype(np.uint8)
-
-    # Convert the HLS to RGB
-    new_hls = cv2.merge((new_h, new_l, new_s)).astype(np.uint8)
-    new_im = cv2.cvtColor(new_hls, cv2.COLOR_HLS2RGB)
-
-    image_color_warped = new_im.astype(np.float32)
-
-    return image_color_warped
-
-
 def png_to_mp4(group: h5py.Group, key: str, name: str, fps=10):
     """
     Write key out as a mpt
@@ -200,16 +132,3 @@ def pngs_to_mp4(filename: str, key: str, vid_name: str, fps: int):
     h5 = h5py.File(filename, "r")
     for group_name, group in h5.items():
         png_to_mp4(group, key, str(vid_name) + "_" + group_name + ".mp4", fps=fps)
-
-
-if __name__ == "__main__":
-    filename = "../hab_stretch/imgs/pile_of_stretches.png"
-    img = Image.open(filename)
-    data = img_to_bytes(np.asarray(img))
-    print("data is now a:", type(data))
-    img2 = img_from_bytes(data)
-
-    import matplotlib.pyplot as plt
-
-    plt.imshow(img2)
-    plt.show()
