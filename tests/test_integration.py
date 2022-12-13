453e2b7ebc8aa7726d03e2f45fe7d62b7ea9081c
ExhAustin <aswang@andrew.cmu.edu>
2022-12-13 12:53:02 -0800

    Update tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -28,14 +28,14 @@ def test_goto(home_robot_stack, robot):
     xyt_goal = [0.1, 0.3, 0.1]
 
     # Activate goto controller & set goal
-    robot.set_nav_mode()
+    robot.switch_to_navigation_mode()
     robot.set_goal(xyt_goal)
 
     # Wait for robot to reach goal
     time.sleep(5)
 
     # Check that robot is at goal
-    xyt_new = robot.get_base_state()
+    xyt_new = robot.get_base_state()["pose_se2"]
 
     assert np.allclose(xyt_new[:2], xyt_goal[:2], atol=0.05)  # 5cm
 
