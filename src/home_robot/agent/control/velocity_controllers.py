772afb02733b0b41af9ae38c1c673e80a43c8bae
ExhAustin <aswang@andrew.cmu.edu>
2022-12-12 21:34:09 -0800

    Tune vel control angular accuracy

--- src/home_robot/agent/control/velocity_controllers.py
+++ src/home_robot/agent/control/velocity_controllers.py
@@ -8,7 +8,7 @@ W_MAX_DEFAULT = 0.45  # (vel_m_max - vel_m_default) / wheel_separation_m
 ACC_LIN = 0.2  # 0.5 * base.params["motion"]["max"]["accel_m"]
 ACC_ANG = 0.6  # 0.5 * (accel_m_max - accel_m_default) / wheel_separation_m
 TOL_LIN = 0.05
-TOL_ANG = 0.2
+TOL_ANG = 0.087  # 5 degrees
 MAX_HEADING_ANG = np.pi / 4
 
 
