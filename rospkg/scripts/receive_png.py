859020975d09623d65e366ba218bf8d0c245f37a
Chris Paxton <cpaxton@fb.com>
2022-12-14 18:43:52 -0500

    update config - planning assets and make scripts run

--- rospkg/scripts/receive_png.py
+++ rospkg/scripts/receive_png.py
@@ -1,11 +1,17 @@
 import rospy
 import imagiz
 import cv2
+from home_robot.hw.ros.msg_numpy import image_to_numpy, numpy_to_image
+from sensor_msgs.msg import Image
 
-server = imagiz.TCP_Server(port=9990)
-server.start()
+color_server = imagiz.TCP_Server(port=9990)
+depth_server = imagiz.TCP_Server(port=9991)
+servers = [color_server, depth_server]
+for server in servers:
+    server.start()
 while True:
-    message = server.receive()
-    frame = cv2.imdecode(message.image, 1)
-    cv2.imshow("", frame)
-    cv2.waitKey(1)
+    for server, name in zip(servers, ["color", "depth"]):
+        message = server.receive()
+        frame = cv2.imdecode(message.image, 1)
+        cv2.imshow(name, frame)
+        cv2.waitKey(1)
