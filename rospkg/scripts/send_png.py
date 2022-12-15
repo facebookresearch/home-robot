2def0f76899d719537bbe354def315fedaf8fc6f
Chris Paxton <cpaxton@fb.com>
2022-12-14 19:06:23 -0500

    fix script formatting

--- rospkg/scripts/send_png.py
+++ rospkg/scripts/send_png.py
@@ -5,24 +5,35 @@ import threading
 import numpy as np
 from home_robot.hw.ros.camera import RosCamera
 
-rospy.init_node('png_sender')
-#client = imagiz.Client("cc1", server_ip="192.168.0.79", server_port=5555)
-color_client = imagiz.TCP_Client(client_name="color", server_ip="192.168.0.79", server_port=9990)
-depth_client = imagiz.TCP_Client(client_name="depth", server_ip="192.168.0.79", server_port=9991)
+# Create setup for clients + cameras
+rospy.init_node("png_sender")
+# client = imagiz.Client("cc1", server_ip="192.168.0.79", server_port=5555)
+color_client = imagiz.TCP_Client(
+    client_name="color", server_ip="192.168.0.79", server_port=9990
+)
+depth_client = imagiz.TCP_Client(
+    client_name="depth", server_ip="192.168.0.79", server_port=9991
+)
 clients = [color_client, depth_client]
 
 color_camera = RosCamera("/camera/color")
 depth_camera = RosCamera("/camera/aligned_depth_to_color")
 cameras = [color_camera, depth_camera]
 
+# Encode everything
 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+
 def encode_color(frame):
     r, image = cv2.imencode(".jpg", frame, encode_param)
     return image
+
+
 def encode_depth(frame):
     frame = (frame * 1000).astype(np.uint16)
     r, image = cv2.imencode(".png", frame)
     return image
+
 
 encoders = [encode_color, encode_depth]
 
@@ -35,4 +46,3 @@ while not rospy.is_shutdown():
         if frame is not None:
             client.send(encode(frame))
     rate.sleep()
-
