d149698d5f50e77f2b16a6515a479916ef089c43
Austin Wang <austinswang@gmail.com>
2023-01-26 14:31:04 -0800

    More licensing stuff for open-sourcing (#27)
    * Add missing headers
    
    * Hello Robot licensing

--- src/home_robot/home_robot/hw/teleop/stretch_xbox_controller_teleop.py
+++ src/home_robot/home_robot/hw/teleop/stretch_xbox_controller_teleop.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 """
 Copied from https://github.com/hello-robot/stretch_body/blob/master/tools/bin/stretch_xbox_controller_teleop.py
+
+Copyright 2020 Hello Robot Inc.
+The Contents are licensed under the Apache License, Version 2.0 (the "License"). You may not use the Contents except in compliance with the License. You may obtain a copy of the License at
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, the Contents are distributed under the License are distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+For further information about the Contents including inquiries about dual licensing, please contact Hello Robot Inc.
 """
 # TODO: move this somewhere better, since it's in the "ros" folder but isn't actually ROS
 
