a424155a7420e1798591aa07eeb95557aad051e3
Theophile Gervet <theophile.gervet@gmail.com>
2023-02-01 20:20:48 -0500

    Interfaces

---
+++ src/home_robot_sim/home_robot_sim/agent/habitat_abstract_agent.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from typing import Any, Union, Dict
+
+import habitat
+from habitat.core.simulator import Observations
+
+
+class HabitatAgent(habitat.Agent, ABC):
+    def __init__(self, home_robot_agent: home_robot.agent.abstract_agent.Agent):
+        self.home_robot_agent = home_robot_agent
+
+    @abstractmethod
+    def reset(self):
+        pass
+
+    @abstractmethod
+    def act(self, obs: Observations) -> Union[int, str, Dict[str, Any]]:
+        pass
