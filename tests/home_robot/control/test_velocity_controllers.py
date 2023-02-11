63b49fd4c4181ebde542c67d580ac752b263986d
ExhAustin <aswang@andrew.cmu.edu>
2023-02-10 21:23:12 -0800

    Add velocity controller test

---
+++ tests/home_robot/control/test_velocity_controllers.py
@@ -0,0 +1,50 @@
+import os
+import json
+import pytest
+
+import numpy as np
+
+from home_robot.control.goto_controller import GotoVelocityController
+
+from utils import generate_controller_input, get_controller_output
+
+HZ = 20
+NUM_ENTRIES = 10
+DATASET_DIR = os.path.dirname(os.path.abspath(__file__))
+DATASET_FILE = "test_xyt_data.json"
+
+
+@pytest.fixture
+def controller():
+    return GotoVelocityController(hz=HZ)
+
+
+@pytest.fixture
+def dataset(controller):
+    dataset_path = os.path.join(DATASET_DIR, DATASET_FILE)
+
+    # If dataset exists: load dataset
+    if os.path.exists(dataset_path):
+        assert os.path.isfile(dataset_path)
+        with open(dataset_path, "r") as f:
+            data = json.load(f)
+
+    # If dataset doesn't exist: Generate dataset and save
+    else:
+        # Generate inputs
+        data = []
+        for _ in range(NUM_ENTRIES):
+            controller_input = generate_controller_input()
+            controller_output = get_controller_output(controller, controller_input)
+            data.append((controller_input, controller_output))
+
+        with open(dataset_path, "w") as f:
+            json.dump(data, f)
+
+    return data
+
+
+def test_controller_input_output(controller, dataset):
+    for x, y_ref in dataset:
+        y = get_controller_output(controller, x)
+        assert np.allclose(y, y_ref)
