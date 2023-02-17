63197ef5cdc2534a9c3940c4e70650ab1f15d174
Chris Paxton <cpaxton@fb.com>
2023-02-17 11:52:19 -0500

    make a bunch of fixes to start teleop scritp

--- src/home_robot_hw/scripts/run_controller_teleop.py
+++ src/home_robot_hw/scripts/run_controller_teleop.py
@@ -16,11 +16,15 @@ if __name__ == "__main__":
     rospy.init_node("xbox_controller")
 
     stretch_planner_urdf_path = get_urdf_dir()
-    print(stretch_planner_urdf_path)
+    # print(stretch_planner_urdf_path)
+    #stretch_planner_urdf_path = os.path.join(
+    #    get_package_path(), "assets/hab_stretch/urdf/planner_calibrated.urdf"
+    #)
+    #print(">>>", stretch_planner_urdf_path)
     model = HelloStretch(
         visualize=False,
         root="",
-        urdf_path=stretch_planner_urdf_path,
+        # urdf_path=stretch_planner_urdf_path,
     )
     controller = StretchXboxController(model)
     rospy.spin()
