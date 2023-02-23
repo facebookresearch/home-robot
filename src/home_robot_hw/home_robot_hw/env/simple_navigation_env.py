9931d29cb158fa3f9c2163e000e9266fef12af40
Priyam Parashar <priyam8parashar@gmail.com>
2023-02-23 13:53:13 -0500

    consolidate driver code into the env definition file

--- src/home_robot_hw/home_robot_hw/env/simple_navigation_env.py
+++ src/home_robot_hw/home_robot_hw/env/simple_navigation_env.py
@@ -22,32 +22,14 @@ class StretchSimpleNavEnv(StretchEnv):
     def reset(self):
         self._current_pose = self.get_base_pose()
 
-    def apply_action(self, action: Action, info: Optional[Dict[str, Any]] = None):
-        if self.visualizer is not None:
-            self.visualizer.visualize(**info)
+    def apply_action(self):
         continuous_action = np.zeros(3)
-        if action == DiscreteNavigationAction.MOVE_FORWARD:
-            print("FORWARD")
-            continuous_action[0] = self.forward_step
-        elif action == DiscreteNavigationAction.TURN_RIGHT:
-            print("TURN RIGHT")
-            continuous_action[2] = -self.rotate_step
-        elif action == DiscreteNavigationAction.TURN_LEFT:
-            print("TURN LEFT")
-            continuous_action[2] = self.rotate_step
-        else:
-            # Do nothing if "stop"
-            # continuous_action = None
-            # if not self.in_manipulation_mode():
-            #     self.switch_to_manipulation_mode()
-            pass
-
+        continuous_action[0] = 0.25
         if continuous_action is not None:
             if not self.in_navigation_mode():
                 self.switch_to_navigation_mode()
             self.navigate_to(continuous_action, relative=True)
         print("-------")
-        print(action)
         print(continuous_action)
         rospy.sleep(5.0)
 
@@ -67,92 +49,9 @@ if __name__ == "__main__":
     print("Start example - hardware using ROS")
     rospy.init_node("hello_stretch_nav_test")
     print("Create ROS interface")
-    rob = StretchSimpleNavEnv()
-    rob.switch_to_navigation_mode()
+    env = StretchSimpleNavEnv()
 
-    # Debug the observation space
-    import matplotlib.pyplot as plt
-
-    while not rospy.is_shutdown():
-
-        while not rospy.is_shutdown():
-            cmd = None
-            try:
-                cmd = input("Enter a number 0-3:")
-                cmd = DiscreteNavigationAction(int(cmd))
-            except ValueError:
-                cmd = None
-            if cmd is not None:
-                break
-        rob.apply_action(cmd)
-
-        obs = rob.get_observation()
-        rgb, depth = obs.rgb, obs.depth
-        # xyt = obs2xyt(obs.base_pose)
-
-        # Add a visualiztion for debugging
-        depth[depth > 5] = 0
-        plt.subplot(121)
-        plt.imshow(rgb)
-        plt.subplot(122)
-        plt.imshow(depth)
-        # plt.subplot(133); plt.imshow(obs.semantic
-
-        print()
-        print("----------------")
-        print("values:")
-        print("RGB =", np.unique(rgb))
-        print("Depth =", np.unique(depth))
-        # print("XY =", xyt[:2])
-        # print("Yaw=", xyt[-1])
-        print("Compass =", obs.compass)
-        print("Gps =", obs.gps)
-        plt.show()
-
-
-if False:
-    observations = []
-    obs = rob.get_observation()
-    observations.append(obs)
-
-    xyt = np.zeros(3)
-    xyt[2] = obs.compass
-    xyt[:2] = obs.gps
-    # xyt = obs2xyt(obs.base_pose)
-    xyt[0] += 0.1
-    # rob.navigate_to(xyt)
-    rob.rotate(0.2)
-    rospy.sleep(10.0)
-    obs = rob.get_observation()
-    observations.append(obs)
-
-    xyt[0] = 0
-    # rob.navigate_to(xyt)
-    rob.rotate(-0.2)
-    rospy.sleep(10.0)
-    obs = rob.get_observation()
-    observations.append(obs)
-
-    for obs in observations:
-        rgb, depth = obs.rgb, obs.depth
-        # xyt = obs2xyt(obs.base_pose)
-
-        # Add a visualiztion for debugging
-        depth[depth > 5] = 0
-        plt.subplot(121)
-        plt.imshow(rgb)
-        plt.subplot(122)
-        plt.imshow(depth)
-        # plt.subplot(133); plt.imshow(obs.semantic
-
-        print()
-        print("----------------")
-        print("values:")
-        print("RGB =", np.unique(rgb))
-        print("Depth =", np.unique(depth))
-        # print("XY =", xyt[:2])
-        # print("Yaw=", xyt[-1])
-        print("Compass =", obs.compass)
-        print("Gps =", obs.gps)
-        plt.show()
-
+    env.reset()
+    env.apply_action()
+    rospy.sleep(1.0)
+    print("Confirm the robot has moved forward")
