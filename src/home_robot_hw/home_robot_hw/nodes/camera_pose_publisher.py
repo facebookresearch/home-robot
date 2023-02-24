67a752226c625c861c4c50cbb8818f751a62b7fd
Chris Paxton <paxton.chris@live.com>
2023-02-24 14:45:08 -0800

    add a camera pose publisher script

---
+++ src/home_robot_hw/home_robot_hw/nodes/camera_pose_publisher.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+import rospy
+import tf
+from geometry_msgs.msg import PoseStamped
+from home_robot.agent.motion.stretch import STRETCH_CAMERA_FRAME
+
+
+class CameraPosePublisher(object):
+    """ publishes the camera pose constantly so that we do not have a dependency on /tf"""
+
+    def __init__(self, topic_name="camera_pose"):
+        self._pub = rospy.Publisher(topic_name, PoseStamped)
+        self._listener = tf.TransformListener()
+
+    def spin(self, rate=10):
+        while not rospy.is_shutdown():
+            try:
+                (trans, rot) = listener.lookupTransform(STRETCH_BASE_FRAME, STRETCH_CAMERA_FRAME, rospy.Time(0))
+                print(trans, rot)
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+
+                continue
+
