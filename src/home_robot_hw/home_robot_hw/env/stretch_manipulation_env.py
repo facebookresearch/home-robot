31f8e1e5a44206b3922ceb01d843091385b6022a
Priyam Parashar <priyam8parashar@gmail.com>
2023-02-28 10:48:15 -0500

    removing unnecessary files added before

--- src/home_robot_hw/home_robot_hw/env/stretch_manipulation_env.py
+++
@@ -1,8 +0,0 @@
-from home_robot_hw.env.stretch_abstract_env import StretchEnv
-
-
-class StretchManipulationEnv(StretchEnv):
-    """Simple manipulation environment"""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
