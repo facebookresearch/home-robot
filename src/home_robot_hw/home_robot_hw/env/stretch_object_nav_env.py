18678866e67e1a44426729dfc1242d73ffe51be8
Chris Paxton <cpaxton@fb.com>
2023-03-01 12:42:37 -0500

    switch to use the real-world coordinate frame in habitat

--- src/home_robot_hw/home_robot_hw/env/stretch_object_nav_env.py
+++ src/home_robot_hw/home_robot_hw/env/stretch_object_nav_env.py
@@ -109,11 +109,11 @@ class StretchObjectNavEnv(StretchEnv):
         # GPS in robot coordinates
         # TODO: this should actually be correct as-is
         # TODO: t his is the correct version
-        # gps = relative_pose.translation()[:2]
-        relative_trans = relative_pose.translation()
+        gps = relative_pose.translation()[:2]
+        # relative_trans = relative_pose.translation()
         # TODO: move into its own function
         # Invert the Y coordinate to make +y right, consistent with habitat
-        gps = np.array([relative_trans[0], -relative_trans[1]])
+        # gps = np.array([relative_trans[0], -relative_trans[1]])
 
         # Create the observation
         obs = home_robot.core.interfaces.Observations(
--- src/home_robot_sim/home_robot_sim/env/habitat_objectnav_env/habitat_objectnav_env.py
+++ src/home_robot_sim/home_robot_sim/env/habitat_objectnav_env/habitat_objectnav_env.py
@@ -77,11 +77,13 @@ class HabitatObjectNavEnv(HabitatEnv):
     ) -> home_robot.core.interfaces.Observations:
         depth = self._preprocess_depth(habitat_obs["depth"])
         goal_id, goal_name = self._preprocess_goal(habitat_obs["objectgoal"])
+        gps = habitat_obs["gps"]
+        gps_fixed = np.array([gps[0], -1 * gps[1]])
         obs = home_robot.core.interfaces.Observations(
             rgb=habitat_obs["rgb"],
             depth=depth,
             compass=habitat_obs["compass"],
-            gps=habitat_obs["gps"],
+            gps=gps_fixed,
             task_observations={
                 "goal_id": goal_id,
                 "goal_name": goal_name,
