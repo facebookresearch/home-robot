a0e908c37c429ffcec11c8ec5ffe94d72b5e92a8
ExhAustin <aswang@andrew.cmu.edu>
2023-03-01 14:26:19 -0800

    Add copyright headers

--- src/home_robot/home_robot/motion/pinocchio_ik_solver.py
+++ src/home_robot/home_robot/motion/pinocchio_ik_solver.py
@@ -1,4 +1,7 @@
-import os
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
 from typing import Callable, List, Tuple, Optional, Dict, Union
 
 import numpy as np
