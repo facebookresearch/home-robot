616a5511a31e1f1fc3cbc8fcde1fe5ad186d3d9a
Chris Paxton <cpaxton@fb.com>
2023-03-02 12:40:46 -0500

    update stretch grasping baseline

---
+++ projects/stretch_grasping/eval_episode.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import rospy
+
+from home_robot.motion.stretch import STRETCH_HOME_Q
+from home_robot.agent.objectnav_agent.objectnav_agent import ObjectNavAgent
+from home_robot.utils.config import get_config
+from home_robot_hw.env.stretch_object_nav_env import StretchGraspingEnv
+
+if __name__ == "__main__":
+    config_path = "configs/agent/floorplanner_eval.yaml"
+    config, config_str = get_config(config_path)
+    config.defrost()
+    config.NUM_ENVIRONMENTS = 1
+    config.PRINT_IMAGES = 1
+    config.EXP_NAME = "debug"
+    config.freeze()
+
+    rospy.init_node("eval_episode_stretch_objectnav")
+    agent = ObjectNavAgent(config=config)
+    env = StretchGraspingEnv(config=config)
+    env.goto(STRETCH_HOME_Q)
+
+    agent.reset()
+    env.reset()
+
+    t = 0
+    while not env.episode_over:
+        t += 1
+        print("STEP =", t)
+        obs = env.get_observation()
+        action, info = agent.act(obs)
+        env.apply_action(action, info=info)
+
+    print(env.get_episode_metrics())
