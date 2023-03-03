facebookresearch/home-robot

commit ac5e70131bbe21e054b6c527785d63b75111ac7b

Porting HDF5 recorder environment along with Pinocchio Solver (#76)
* move writing to file to the end to avoid corruption

* skeleton env for manipulation; do i need this?

* porting in pinochhio IK solver for manipulation

* bug

* porting demo envs from Sam

* port optim based IK

* integrating pinochhio ik solver into HelloStretch

* environments to work with h5 demonstrations

* cleaner code

* check _ik_type before invoking call to solver

* add close func

* initialize pose as None

* add docstring

* pb_ik_opt_solver-->compute_ik_opt

* bug-fix:urdf path, list->List

* override abstract methods

* initialize ROS node before creating pub-subs

* rectifying hardcoded defaults

* simple script to collect demonstrations as H5s

* Multiple changes:

* Moved camera-info to config
* Added ee-pose and base-pose to each frame
* Added meta-variables to track recording

* removing unnecessary files added before

* create recorder once at init-time

* add type-hints

* added ee-pose, base-pose; moved cam-info to config

* type hints

* python standards, 2 blank lines before main

* docstring

* append to h5 file, don't rewrite it

* type hints

* add user_keyframe key to H5

* type hints, better defaults, better conditionals, raise ValueError

* Dict[str]

* docstrings, types, descriptions

* description of vars + dir structure

* type hints + docstrings where missing

* driver code moved to tests

* resolve import problems; TODOs for writing test

* return more stats for debugging

* code works; test passes

* unifying calls with pybullet ik calls

* manipulate output specific to pinocchio ik

* added test for IK/FK consistency from test_ik.py

* move constants to stretch.py

* addes tests for consistency of FK+IK b/w the two solvers

* Modularize IK optimizer

* Debug

* move to new motion folder

* fixed imports

* Combine ik tests with parametrized fixtures

src/home_robot/home_robot/motion/stretch.py
@@ -3,13 +3,14 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 import os
+from typing import Tuple
 
 import numpy as np
 import pybullet as pb
 import trimesh.transformations as tra
 
 import home_robot.utils.bullet as hrb
-
+from home_robot.motion.pinocchio_ik_solver import PinocchioIKSolver
 from home_robot.motion.robot import Robot
 from home_robot.utils.bullet import PybulletIKSolver
 from home_robot.utils.pose import to_matrix
@@ -65,6 +66,17 @@ STRETCH_NAVIGATION_Q = np.array(
         -np.pi / 4,
     ]
 )
+PIN_CONTROLLED_JOINTS = [
+    # "base_x_joint",
+    "joint_lift",
+    "joint_arm_l0",
+    "joint_arm_l1",
+    "joint_arm_l2",
+    "joint_arm_l3",
+    "joint_wrist_yaw",
+    "joint_wrist_pitch",
+    "joint_wrist_roll",
+]
 
 
 # This is the gripper, and the distance in the gripper frame to where the fingers will roughly meet
@@ -168,7 +180,7 @@ class HelloStretch(Robot):
         "joint_wrist_roll",
     ]
 
-    def _create_ik_solvers(self):
+    def _create_ik_solvers(self, ik_type: str = "pybullet"):
         """create ik solvers using pybullet"""
         # You can set one of the visualize flags to true to debug IK issues
         # This is not exposed manually - only one though or it will fail
@@ -179,18 +191,34 @@ class HelloStretch(Robot):
             visualize=False,
         )
         # You can set one of the visualize flags to true to debug IK issues
-        self.manip_ik_solver = PybulletIKSolver(
-            self.manip_mode_urdf_path,
-            self.ee_link_name,
-            self.manip_mode_controlled_joints,
-            visualize=False,
-        )
+        if ik_type == "pybullet":
+            self.manip_ik_solver = PybulletIKSolver(
+                self.manip_mode_urdf_path,
+                self.ee_link_name,
+                self.manip_mode_controlled_joints,
+                visualize=False,
+            )
+        elif ik_type == "pinocchio":
+            self.manip_ik_solver = PinocchioIKSolver(
+                self.manip_mode_urdf_path,
+                self.ee_link_name,
+                self.manip_mode_controlled_joints,
+            )
+        else:
+            raise ValueError(f"Unknown ik_type {ik_type}")
 
-    def __init__(self, name="robot", urdf_path=None, visualize=False, root="."):
+    def __init__(
+        self,
+        name: str = "robot",
+        urdf_path: str = "",
+        visualize: bool = False,
+        root: str = ".",
+        ik_type: str = "pybullet",
+    ):
         """Create the robot in bullet for things like kinematics; extract information"""
 
         # urdf
-        if urdf_path is None:
+        if not urdf_path:
             full_body_urdf = PLANNER_STRETCH_URDF
             manip_urdf = MANIP_STRETCH_URDF
         else:
@@ -218,7 +246,8 @@ class HelloStretch(Robot):
         self.set_joint_position = self.ref.set_joint_position
 
         self._update_joints()
-        self._create_ik_solvers()
+        self._create_ik_solvers(ik_type=ik_type)
+        self._ik_type = ik_type
 
     def set_head_config(self, q):
         # WARNING: this sets all configs
@@ -435,14 +464,19 @@ class HelloStretch(Robot):
             self.set_config(q)
         return self.ref.get_link_pose(link_name)
 
-    def fk(self, q=None, as_matrix=False):
+    def fk(self, q=None, as_matrix=False) -> Tuple[np.ndarray, np.ndarray]:
         """forward kinematics"""
-        pose = self.get_link_pose(self.ee_link_name, q)
+        pose = None
+        if self._ik_type == "pybullet":
+            pose = self.get_link_pose(self.ee_link_name, q)
+        elif self._ik_type == "pinocchio":
+            pose = self.ik_solver.compute_fk(q)
         if as_matrix:
             return to_matrix(*pose)
         return pose
 
-    def update_head(self, qi, look_at):
+    def update_head(self, qi: np.ndarray, look_at) -> np.ndarray:
+        """move head based on look_at and return the joint-state"""
         qi[HelloStretchIdx.HEAD_PAN] = look_at[0]
         qi[HelloStretchIdx.HEAD_TILT] = look_at[1]
         return qi
@@ -547,7 +581,7 @@ class HelloStretch(Robot):
         pose[:3, :3] = np.array(se3).reshape(3, 3)
         x, y, z = pos
         pose[:3, 3] = np.array([x, y, z - self.base_height])
-        q = self.ik_solver.ik(pose, self._to_ik_format(q0))
+        q = self.ik_solver.compute_ik(pose, self._to_ik_format(q0))
         if q is not None:
             return self._to_plan_format(q)
         else:
@@ -574,6 +608,8 @@ class HelloStretch(Robot):
             # This logic currently in local hello robot client
             raise NotImplementedError()
         _q = self.manip_ik_solver.compute_ik(pos, quat, self._to_manip_format(q0))
+        if self._ik_type == "pinocchio":
+            _q = _q[0]
         q = self._from_manip_format(_q, q0)
         self.set_config(q)
         return q

projects/collect_h5.py
@@ -1,0 +1,86 @@
+import os
+from datetime import datetime
+
+import click
+import rospy
+
+from home_robot.agent.motion.stretch import HelloStretch
+from home_robot_hw.ros.path import get_urdf_dir
+from home_robot_hw.ros.recorder import Recorder
+from home_robot_hw.teleop.stretch_xbox_controller import StretchXboxController
+
+
+class EpisodeManager(object):
+    """Episode manager class for creating, closing and managing episodic data
+    This works in a task-centric fashion right now. Creating or using the
+    task-directory inside ~/H5s/ and adding new files with today's date
+    and time.
+    """
+
+    def __init__(self, task_name: str, dir_path: str = os.path.expanduser("~/H5s/")):
+        """
+        task_name: name of the task to record, string
+        dir_path: path to the directory where the task directory will be created
+
+        The resulting directory structure is as follows:
+            dir_path
+            |--task_name_0
+            |  |--date_time_string_0.h5
+            |  |--date_time_string_1.h5
+            |--task_name_1
+            |  |--date_time_string_0.h5
+            ...
+        A new date_time_string file is created whenever this script is run for a task
+        """
+        stretch_planner_urdf_path = get_urdf_dir()
+        self.model = HelloStretch(
+            visualize=False,
+            root="",
+            urdf_path=stretch_planner_urdf_path,
+        )
+        self.task_name = task_name
+        self.file_path = os.path.join(dir_path, task_name)
+        os.makedirs(self.file_path, exist_ok=True)
+        self.controller = StretchXboxController(
+            self.model,
+            start_button_callback=self.toggle_episode,
+            back_button_callback=self.record_keyframe,
+        )
+        self._is_recording = False
+        date_time_string = datetime.now().strftime("%m-%d_%H-%M-%S")
+        filename = os.path.join(self.file_path, date_time_string)
+        self._keyframe_recorder = Recorder(filename)
+
+    def toggle_episode(self):
+        """toggles episode recording state
+        Note: This script automatically collects a keyframe at the end when
+        episode is terminated. It doesn't require the last frame to be explicitly marked as a keyframe
+        """
+        if not self._is_recording:
+            self._is_recording = True
+            self._keyframe_recorder.start_recording(self.task_name)
+        else:
+            self._is_recording = False
+            self._keyframe_recorder.save_frame()
+            self._keyframe_recorder.finish_recording()
+
+    def record_keyframe(self):
+        """add a keyframe to the current episode"""
+        self._keyframe_recorder.save_frame(is_keyframe=True)
+
+
+@click.command()
+@click.option("--task-name", default="task", help="Name of the task to record")
+def main(task_name):
+    rospy.init_node("h5_demo_recorder")
+    rate = rospy.Rate(10)
+    data_directory = os.path.expanduser("~/H5s/")
+    em = EpisodeManager(task_name, data_directory)
+    while not rospy.is_shutdown():
+        rospy.spin()
+        rate.sleep()
+    print("Shutting down h5 demo recording script")
+
+
+if __name__ == "__main__":
+    main()

src/home_robot/environment.yml
@@ -6,6 +6,7 @@ dependencies:
   - pybind11
   - pytorch
   - torchvision
+  - pinocchio
   - pip:
     - numpy <1.24 # certain deprecated operations were used in other deps
     - scipy

src/home_robot/home_robot/motion/pinocchio_ik_solver.py
@@ -1,0 +1,263 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+from typing import Callable, List, Tuple, Optional, Dict, Union
+
+import numpy as np
+import pinocchio
+from scipy.spatial.transform import Rotation as R
+
+from home_robot.utils.bullet import PybulletIKSolver
+
+# --DEFAULTS--
+# Error tolerances
+POS_ERROR_TOL = 0.005
+ORI_ERROR_TOL = [0.1, 0.1, np.pi / 2]
+
+# CEM
+CEM_MAX_ITERATIONS = 5
+CEM_NUM_SAMPLES = 50
+CEM_NUM_TOP = 10
+
+
+class PinocchioIKSolver:
+    """IK solver using pinocchio which can handle end-effector constraints for optimized IK solutions"""
+
+    EPS = 1e-4
+    DT = 1e-1
+    DAMP = 1e-12
+
+    def __init__(self, urdf_path: str, ee_link_name: str, controlled_joints: List[str]):
+        """
+        urdf_path: path to urdf file
+        ee_link_name: name of the end-effector link
+        controlled_joints: list of joint names to control
+        """
+        self.model = pinocchio.buildModelFromUrdf(urdf_path)
+        self.data = self.model.createData()
+        self.q_neutral = pinocchio.neutral(self.model)
+
+        self.ee_frame_idx = [f.name for f in self.model.frames].index(ee_link_name)
+        self.controlled_joints = [
+            self.model.idx_qs[self.model.getJointId(j)] if j != "ignore" else -1
+            for j in controlled_joints
+        ]
+
+    def get_dof(self) -> int:
+        """returns dof for the manipulation chain"""
+        return len(self.controlled_joints)
+
+    def get_num_controllable_joints(self) -> int:
+        """returns number of controllable joints under this solver's purview"""
+        return len(self.controlled_joints)
+
+    def _qmap_control2model(self, q_input: np.ndarray) -> np.ndarray:
+        """returns a full joint configuration from a partial joint configuration"""
+        q_out = self.q_neutral.copy()
+        for i, joint_idx in enumerate(self.controlled_joints):
+            q_out[joint_idx] = q_input[i]
+
+        return q_out
+
+    def _qmap_model2control(self, q_input: np.ndarray) -> np.ndarray:
+        """returns a partial joint configuration from a full joint configuration"""
+        q_out = np.empty(len(self.controlled_joints))
+        for i, joint_idx in enumerate(self.controlled_joints):
+            if joint_idx >= 0:
+                q_out[i] = q_input[joint_idx]
+
+        return q_out
+
+    def compute_fk(self, q) -> Tuple[np.ndarray, np.ndarray]:
+        """given joint values return end-effector position and quaternion associated with it"""
+        q_model = self._qmap_control2model(q)
+        pinocchio.forwardKinematics(self.model, self.data, q_model)
+        pinocchio.updateFramePlacement(self.model, self.data, self.ee_frame_idx)
+        pos = self.data.oMf[self.ee_frame_idx].translation
+        quat = R.from_matrix(self.data.oMf[self.ee_frame_idx].rotation).as_quat()
+
+        return pos.copy(), quat.copy()
+
+    def compute_ik(
+        self, pos: np.ndarray, quat: np.ndarray, q_init=None, max_iterations=100
+    ) -> Tuple[np.ndarray, bool]:
+        """given end-effector position and quaternion, return joint values"""
+        i = 0
+        if q_init is None:
+            q = self.q_neutral.copy()
+        else:
+            q = self._qmap_control2model(q_init)
+        desired_ee_pose = pinocchio.SE3(R.from_quat(quat).as_matrix(), pos)
+        while True:
+            pinocchio.forwardKinematics(self.model, self.data, q)
+            pinocchio.updateFramePlacement(self.model, self.data, self.ee_frame_idx)
+
+            dMi = desired_ee_pose.actInv(self.data.oMf[self.ee_frame_idx])
+            err = pinocchio.log(dMi).vector
+            if np.linalg.norm(err) < self.EPS:
+                success = True
+                break
+            if i >= max_iterations:
+                success = False
+                break
+            J = pinocchio.computeFrameJacobian(
+                self.model,
+                self.data,
+                q,
+                self.ee_frame_idx,
+                pinocchio.ReferenceFrame.LOCAL,
+            )
+            v = -J.T.dot(np.linalg.solve(J.dot(J.T) + self.DAMP * np.eye(6), err))
+            q = pinocchio.integrate(self.model, q, v * self.DT)
+            i += 1
+
+        q_control = self._qmap_model2control(q.flatten())
+
+        return q_control, success
+
+
+class PositionIKOptimizer:
+    """
+    Solver that jointly optimizes IK and best orientation to achieve desired position
+    """
+
+    max_iterations: int = 30  # Max num of iterations for CEM
+    num_samples: int = 100  # Total candidate samples for each CEM iteration
+    num_top: int = 10  # Top N candidates for each CEM iteration
+
+    def __init__(
+        self,
+        ik_solver: Union[PinocchioIKSolver, PybulletIKSolver],
+        pos_error_tol: float,
+        ori_error_range: Union[float, np.ndarray],
+        pos_weight: float = 1.0,
+        ori_weight: float = 0.0,
+        cem_params: Optional[Dict] = None,
+    ):
+        self.pos_wt = pos_weight
+        self.ori_wt = ori_weight
+
+        # Initialize IK solver
+        self.ik_solver = ik_solver
+
+        # Initialize optimizer
+        self.pos_error_tol = pos_error_tol
+        if type(ori_error_range) is float:
+            self.ori_error_range = ori_error_range * np.ones(3)
+        else:
+            self.ori_error_range = ori_error_range
+
+        cem_params = {} if cem_params is None else cem_params
+        max_iterations = (
+            cem_params["max_iterations"]
+            if "max_iterations" in cem_params
+            else self.max_iterations
+        )
+        num_samples = (
+            cem_params["num_samples"]
+            if "num_samples" in cem_params
+            else self.num_samples
+        )
+        num_top = cem_params["num_top"] if "num_top" in cem_params else self.num_top
+
+        self.opt = CEM(
+            max_iterations=max_iterations,
+            num_samples=num_samples,
+            num_top=num_top,
+            tol=self.pos_error_tol,
+            sigma0=self.ori_error_range / 2,
+        )
+
+    def compute_ik_opt(self, pose_query: Tuple[np.ndarray, np.ndarray]):
+        """optimization-based IK solver using CEM"""
+        pos_desired, quat_desired = pose_query
+
+        # Function to optimize: IK error given delta from original desired orientation
+        def solve_ik(dr):
+            pos = pos_desired
+            quat = (R.from_rotvec(dr) * R.from_quat(quat_desired)).as_quat()
+
+            q, _ = self.ik_solver.compute_ik(pos, quat)
+            pos_out, rot_out = self.ik_solver.compute_fk(q)
+
+            cost_pos = np.linalg.norm(pos - pos_out)
+            cost_rot = (
+                1 - (rot_out * quat_desired).sum() ** 2
+            )  # TODO: just minimize dr?
+
+            cost = self.pos_wt * cost_pos + self.ori_wt * cost_rot
+
+            return cost, q
+
+        # Optimize for IK and best orientation (x=0 -> use original desired orientation)
+        cost_opt, q_result, max_iter, opt_sigma = self.opt.optimize(
+            solve_ik, x0=np.zeros(3)
+        )
+        pos_out, quat_out = self.ik_solver.compute_fk(q_result)
+        print(
+            f"After ik optimization, cost: {cost_opt}, result: {pos_out, quat_out} vs desired: {pose_query}"
+        )
+        return q_result, cost_opt, max_iter, opt_sigma
+
+
+class CEM:
+    """class implementing generic CEM solver for optimization"""
+
+    def __init__(
+        self,
+        max_iterations: int,
+        num_samples: int,
+        num_top: int,
+        tol: float,
+        sigma0: np.ndarray,
+    ):
+        """
+        max_iterations: max number of iterations
+        num_samples: number of samples per iteration
+        num_top: number of top samples to use for next iteration
+        tol: tolerance for stopping criterion
+        """
+        self.max_iterations = max_iterations
+        self.num_samples = num_samples
+        self.num_top = num_top
+        self.cost_tol = tol
+        self.sigma0 = sigma0
+
+    def optimize(self, func: Callable, x0: np.ndarray):
+        """optimize function func with initial guess mu=x0 and initial std=sigma0"""
+        assert (
+            x0.shape == self.sigma0.shape
+        ), f"x0 and sigma0 must have same shape, got {x0.shape} and {self.sigma0.shape}"
+
+        i = 0
+        mu = x0
+        sigma = self.sigma0
+        while True:
+            # Sample x
+            x_arr = mu + sigma * np.random.randn(self.num_samples, x0.shape[0])
+
+            # Compute costs
+            cost_arr = np.zeros(self.num_samples)
+            aux_outputs = [None for _ in range(self.num_samples)]
+            for j, x in enumerate(x_arr):
+                cost_arr[j], aux_outputs[j] = func(x)
+
+            # Sort costs
+            idx_sorted_arr = np.argsort(cost_arr)
+            i_best = idx_sorted_arr[0]
+
+            # Check termination
+            i += 1
+            if (
+                i >= self.max_iterations
+                or cost_arr[i_best] <= self.cost_tol
+                or np.all(sigma <= self.cost_tol / 10)
+            ):  # TODO: sigma thresh?
+                break
+
+            # Update distribution
+            mu = np.mean(x_arr[idx_sorted_arr[: self.num_top], :], axis=0)
+            sigma = np.std(x_arr[idx_sorted_arr[: self.num_top], :], axis=0)
+
+        return cost_arr[i_best], aux_outputs[i_best], i, sigma

src/home_robot/home_robot/utils/data_tools/writer.py
@@ -3,6 +3,7 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 import os
+from typing import Dict
 
 import h5py
 import numpy as np
@@ -34,7 +35,6 @@ class DataWriter(object):
             filename = os.path.join(self.dirname, self.filename)
         else:
             filename = self.filename
-        self.h5 = h5py.File(filename, "w")
         self.num_trials = 0
         self.reset()
 
@@ -78,7 +78,7 @@ class DataWriter(object):
             self.temporal_data[k].append(v)
         return True
 
-    def fix_data(self, data):
+    def fix_data(self, data) -> Dict[str]:
         """Flatten dictionaries"""
         new_data = {}
         for k, v in data.items():
@@ -91,7 +91,7 @@ class DataWriter(object):
                 new_data[k] = v
         return new_data
 
-    def flatten_dict(self, key, data):
+    def flatten_dict(self, key, data) -> Dict[str]:
         """Take a dictionary (data) and turn it into a flat dict, with keys separated by
         slashes as per os.path.join"""
         new_data = {}
@@ -135,37 +135,38 @@ class DataWriter(object):
         else:
             trial_id = str(trial_id)
         self.num_trials += 1
-        trial = self.h5.create_group(trial_id)
+        with h5py.File(self.filename, "a") as h5_file:
+            trial = h5_file.create_group(trial_id)
 
-        # Now write the example out here
-        temporal_keys = ",".join(list(self.temporal_data.keys()))
-        img_keys = ",".join(list(self.img_data.keys()))
-        config_keys = ",".join(list(self.config_data.keys()))
-        data = self.temporal_data
-        data.update(self.config_data)
-        for k, v in data.items():
-            # Add this to the hdf5 file
-            if k[-1] == "_":
-                raise RuntimeError(
-                    "invalid name for dataset key: "
-                    + str(key)
-                    + " cannot end with _; this is reserved."
-                )
-            try:
-                trial[k] = v
-            except TypeError as e:
-                print(e)
-                print("Cannot use type: ", k)
-                import pdb
+            # Now write the example out here
+            temporal_keys = ",".join(list(self.temporal_data.keys()))
+            img_keys = ",".join(list(self.img_data.keys()))
+            config_keys = ",".join(list(self.config_data.keys()))
+            data = self.temporal_data
+            data.update(self.config_data)
+            for k, v in data.items():
+                # Add this to the hdf5 file
+                if k[-1] == "_":
+                    raise RuntimeError(
+                        "invalid name for dataset key: "
+                        + str(k)
+                        + " cannot end with _; this is reserved."
+                    )
+                try:
+                    trial[k] = v
+                except TypeError as e:
+                    print(e)
+                    print("Cannot use type: ", k)
+                    import pdb
 
-                pdb.set_trace()
-        for k, v in self.img_data.items():
-            for i, bindata in enumerate(v):
-                ki = os.path.join(k, str(i))
-                trial[ki] = np.void(bindata)
-        trial[base.TEMPORAL_KEYS] = temporal_keys
-        trial[base.CONFIG_KEYS] = config_keys
-        trial[base.IMAGE_KEYS] = img_keys
+                    pdb.set_trace()
+            for k, v in self.img_data.items():
+                for i, bindata in enumerate(v):
+                    ki = os.path.join(k, str(i))
+                    trial[ki] = np.void(bindata)
+            trial[base.TEMPORAL_KEYS] = temporal_keys
+            trial[base.CONFIG_KEYS] = config_keys
+            trial[base.IMAGE_KEYS] = img_keys
 
         # At the end, clear current stored data + configs
         self.reset()

src/home_robot/home_robot/utils/pose.py
@@ -84,7 +84,8 @@ def to_pos_quat(matrix):
     return pos, np.array([x, y, z, w])
 
 
-def to_matrix(pos, rot):
+def to_matrix(pos, rot) -> np.ndarray:
+    """converts pos, quat to matrix format"""
     x, y, z, w = rot
     T = tra.quaternion_matrix([w, x, y, z])
     T[:3, 3] = pos

src/home_robot_hw/home_robot_hw/ros/recorder.py
@@ -8,38 +8,47 @@ import cv2
 import h5py
 import numpy as np
 import rospy
-from tqdm import tqdm
-
 from home_robot.utils.data_tools.image import img_from_bytes
 from home_robot.utils.data_tools.writer import DataWriter
+from tqdm import tqdm
+
 from home_robot_hw.ros.stretch_ros import HelloStretchROSInterface
 
 
 class Recorder(object):
     """ROS object that subscribes from information from the robot and publishes it out."""
 
-    def __init__(self, filename, start_recording=True, model=None, robot=None):
+    def __init__(self, filename, start_recording=False, model=None, robot=None):
         """Collect information"""
-        print("robot")
+        print("Connecting to robot environment...")
         self.robot = (
             HelloStretchROSInterface(visualize_planner=False, model=model)
             if robot is None
             else robot
         )
-        print("done")
-        print("done")
+        print("... done connecting to robot environment")
         self.rgb_cam = self.robot.rgb_cam
         self.dpt_cam = self.robot.dpt_cam
         self.writer = DataWriter(filename)
         self.idx = 0
-        self._recording_started = False
+        self._recording_started = start_recording
+        self._filename = filename
 
-    def start_recording(self):
-        print("Starting to record...")
+    def start_recording(self, task_name):
         self._recording_started = True
+        # add camera-info as config
+        color_camera_info = self._construct_camera_info(self.robot.rgb_cam)
+        depth_camera_info = self._construct_camera_info(self.robot.dpt_cam)
+        self.writer.add_config(
+            color_camera_info=color_camera_info,
+            depth_camera_info=depth_camera_info,
+            task_name=task_name,
+        )
+
+        print(f"Ready to record demonstration to file: {self._filename}")
 
     def finish_recording(self):
-        print("... done recording.")
+        print(f"... done recording trial named: {self.idx}.")
         self.writer.write_trial(self.idx)
         self.idx += 1
 
@@ -52,21 +61,45 @@ class Recorder(object):
             "P": camera.P,
         }
 
-    def save_frame(self):
+    def save_frame(self, is_keyframe=False):
+        """saves the following to an H5 file:
+        1. rgb image
+        2. depth image
+        3. base pose
+        4. joint positions
+        5. joint velocities
+        6. camera pose
+        7. camera info
+        8. end-effector pose
+        """
         # record rgb and depth
         rgb, depth = self.robot.get_images(compute_xyz=False)
         q, dq = self.robot.update()
-        color_camera_info = self._construct_camera_info(self.robot.rgb_cam)
-        depth_camera_info = self._construct_camera_info(self.robot.dpt_cam)
+        # TODO get the following from TF lookup
+        ee_pose = self.robot.model.fk(q)
+        # output of above is a tuple of two ndarrays
+        # ee-pose should be 1 ndarray of 7 values
+        ee_pose = np.concatenate((ee_pose[0], ee_pose[1]), axis=0)
+        # elements in following are of type: Tuple(Tuple(x,y,theta), rospy.Time)
+        # change to ndarray with 4 floats
+        base_pose = self.robot.get_base_pose()
+        base_pose = np.array(
+            [base_pose[0][0], base_pose[0][1], base_pose[0][2], base_pose[1].to_sec()]
+        )
         camera_pose = self.robot.get_camera_pose()
+        if is_keyframe:
+            user_keyframe = np.array([1])
+        else:
+            user_keyframe = np.array([0])
         self.writer.add_img_frame(rgb=rgb, depth=(depth * 10000).astype(np.uint16))
         self.writer.add_frame(
             q=q,
             dq=dq,
-            color_camera_info=color_camera_info,
-            depth_camera_info=depth_camera_info,
+            ee_pose=ee_pose,
+            base_pose=base_pose,
             camera_pose=camera_pose,
-        )  # TODO: camera info every frame...? Probably not necessary
+            user_keyframe=user_keyframe,
+        )
 
         return rgb, depth, q, dq
 
@@ -80,6 +113,10 @@ class Recorder(object):
             self.save_frame()
             rate.sleep()
         self.finish_recording()
+
+    def close(self):
+        """clean-up: delete self"""
+        del self
 
 
 def png_to_mp4(group: h5py.Group, key: str, name: str, fps=10):
@@ -96,7 +133,6 @@ def png_to_mp4(group: h5py.Group, key: str, name: str, fps=10):
         sorted([(int(j), j) for j in img_stream.keys()], key=lambda pair: pair[0]),
         ncols=50,
     ):
-
         bindata = img_stream[k][()]
         _img = img_from_bytes(bindata)
         w, h = _img.shape[:2]

commit 72d1aede663a9aee29ac26c7a28f713dda5478ca

resolve bug introduced in #76

src/home_robot/home_robot/motion/stretch.py
@@ -467,10 +467,7 @@ class HelloStretch(Robot):
     def fk(self, q=None, as_matrix=False) -> Tuple[np.ndarray, np.ndarray]:
         """forward kinematics"""
         pose = None
-        if self._ik_type == "pybullet":
-            pose = self.get_link_pose(self.ee_link_name, q)
-        elif self._ik_type == "pinocchio":
-            pose = self.ik_solver.compute_fk(q)
+        pose = self.get_link_pose(self.ee_link_name, q)
         if as_matrix:
             return to_matrix(*pose)
         return pose