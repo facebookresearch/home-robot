32e04e69baa64be53749f175a593374bd13ad42f
Chris Paxton <cpaxton@fb.com>
2023-03-08 11:48:02 -0500

    add "simple" agent for grasping

--- src/home_robot_hw/home_robot_hw/env/stretch_grasping_env.py
+++ src/home_robot_hw/home_robot_hw/env/stretch_grasping_env.py
@@ -6,12 +6,14 @@ from home_robot.core.interfaces import Action, DiscreteNavigationAction, Observa
 from home_robot.perception.detection.detic.detic_perception import DeticPerception
 from home_robot_hw.env.stretch_abstract_env import StretchEnv
 
+# REAL_WORLD_CATEGORIES = ["other", "chair", "mug", "other",]
+# REAL_WORLD_CATEGORIES = ["other", "backpack", "other",]
 REAL_WORLD_CATEGORIES = [
     "other",
-    "cup",
-    "other",
+    "chair",
+    "mug",
+    "table" "other",
 ]
-
 
 DETIC = "detic"
 
@@ -19,11 +21,21 @@ DETIC = "detic"
 class StretchGraspingEnv(StretchEnv):
     """Create a Detic-based grasping environment"""
 
-    def __init__(self, segmentation_method=DETIC, *args, **kwargs):
+    def __init__(
+        self,
+        config=None,
+        forward_step=0.25,
+        rotate_step=30.0,
+        segmentation_method=DETIC,
+        *args,
+        **kwargs
+    ):
         super().__init__(*args, **kwargs)
 
         # TODO: pass this in or load from cfg
         self.goal_options = REAL_WORLD_CATEGORIES
+        self.forward_step = forward_step  # in meters
+        self.rotate_step = np.radians(rotate_step)
 
         self.segmentation_method = segmentation_method
         if self.segmentation_method == DETIC:
@@ -33,6 +45,12 @@ class StretchGraspingEnv(StretchEnv):
                 custom_vocabulary=",".join(self.goal_options),
                 sem_gpu_id=0,
             )
+
+        if config is not None:
+            self.visualizer = Visualizer(config)
+        else:
+            self.visualizer = None
+        self.reset()
 
     def reset(self):
         # TODO Make this better
@@ -59,28 +77,59 @@ class StretchGraspingEnv(StretchEnv):
             # continuous_action = None
             # if not self.in_manipulation_mode():
             #     self.switch_to_manipulation_mode()
+            pass
 
         if continuous_action is not None:
             if not self.in_navigation_mode():
                 self.switch_to_navigation_mode()
                 rospy.sleep(self.msg_delay_t)
             self.navigate_to(continuous_action, relative=True, blocking=True)
+
+        # Sleep after sending the navigate command
         rospy.sleep(0.5)
 
+    def set_goal(self, goal):
+        """set a goal as a string"""
+        if goal in self.goal_options:
+            self.current_goal_id = self.goal_options.index(goal)
+            self.current_goal_name = goal
+            return True
+        else:
+            return False
+
+    def sample_goal(self):
+        """set a random goal"""
+        # idx = np.random.randint(len(self.goal_options) - 2) + 1
+        idx = 2
+        self.current_goal_id = idx
+        self.current_goal_name = self.goal_options[idx]
+
     def get_observation(self) -> Observations:
-        rgb, depth, xyz = self.get_images(compute_xyz=True, rotate_images=True)
+        """Get Detic and rgb/xyz/theta from this"""
+        rgb, depth = self.get_images(compute_xyz=False, rotate_images=True)
+        current_pose = xyt2sophus(self.get_base_pose())
+
+        # use sophus to get the relative translation
+        relative_pose = self._episode_start_pose.inverse() * current_pose
+        euler_angles = relative_pose.so3().log()
+        theta = euler_angles[-1]
+        # pos, vel, frc = self.get_joint_state()
+
+        # GPS in robot coordinates
+        gps = relative_pose.translation()[:2]
 
         # Create the observation
-        obs = Observations(
+        obs = home_robot.core.interfaces.Observations(
             rgb=rgb.copy(),
             depth=depth.copy(),
-            xyz=xyz.copy(),
-            gps=np.zeros(2),  # TODO Replace
-            compass=np.zeros(1),  # TODO Replace
+            gps=gps,
+            compass=np.array([theta]),
+            # base_pose=sophus2obs(relative_pose),
             task_observations={
                 "goal_id": self.current_goal_id,
                 "goal_name": self.current_goal_name,
             },
+            # joint_positions=pos,
         )
         # Run the segmentation model here
         if self.segmentation_method == DETIC:
