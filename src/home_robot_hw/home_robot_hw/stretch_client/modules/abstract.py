876c55913c13ec920dacc52b959f663e1a377580
ExhAustin <aswang@andrew.cmu.edu>
2023-03-09 14:18:52 -0800

    Add comments

--- src/home_robot_hw/home_robot_hw/stretch_client/modules/abstract.py
+++ src/home_robot_hw/home_robot_hw/stretch_client/modules/abstract.py
@@ -32,14 +32,21 @@ class AbstractControlModule(abc.ABC):
         self._wait_threads = []
 
     def _register_wait(self, func):
+        """
+        Add a wait function that returns when actions have ended.
+        The function will be run in a separate thread to not block users.
+        is_busy() and wait() methods are available to interact with the thread.
+        """
         thr = threading.Thread(target=func)
         self._wait_threads.append()
         thr.start()
 
     def is_busy(self):
+        """Check if any action is undergoing"""
         return bool(self._busy_threads)
 
     def wait(self):
+        """Wait for all action threads to complete"""
         while self._wait_threads:
             thr = self._wait_threads.pop()
             thr.join()
