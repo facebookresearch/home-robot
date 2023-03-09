12552553316b1eb145033540682d2f15ea0edd02
Chris Paxton <cpaxton@fb.com>
2023-03-09 14:54:42 -0500

    add extra info to the 3d mapper tool

--- src/home_robot/home_robot/mapping/voxel.py
+++ src/home_robot/home_robot/mapping/voxel.py
@@ -38,12 +38,12 @@ class SparseVoxelMap(object):
         self.feats = None
         self.observations = []
 
-    def add(self, camera_pose: np.ndarray, xyz: np.ndarray, feats: np.ndarray):
+    def add(self, camera_pose: np.ndarray, xyz: np.ndarray, feats: np.ndarray, **info):
         """Add this to our history of observations. Also update the current running map."""
         assert xyz.shape[-1] == 3
         assert feats.shape[-1] == self.feature_dim
         assert xyz.shape[0] == feats.shape[0]
-        self.observations.append((camera_pose, xyz, feats))
+        self.observations.append((camera_pose, xyz, feats, info))
         world_xyz = trimesh.transform_points(xyz, camera_pose)
 
         if self.xyz is None:
@@ -71,7 +71,7 @@ class SparseVoxelMap(object):
     def recompute_map(self):
         """Recompute the entire map from scratch instead of doing incremental updates"""
         self.xyz, self.feats = None, None
-        for camera_pose, xyz, feats in self.observations:
+        for camera_pose, xyz, feats, _ in self.observations:
             world_xyz = trimesh.transform_points(xyz, camera_pose)
             if self.xyz is None:
                 self.xyz, self.feats = world_xyz, feats
