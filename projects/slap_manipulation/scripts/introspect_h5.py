a303258e4add38130b4fe6b144321d5fd7c1387a
Priyam Parashar <priyam8parashar@gmail.com>
2023-03-09 10:37:01 -0500

    script to introspect trials in an h5-file

--- projects/slap_manipulation/scripts/introspect_h5.py
+++ projects/slap_manipulation/scripts/introspect_h5.py
@@ -1,11 +1,27 @@
+import click
 import h5py
 import rospy
 import tf2_ros
-from slap_manipulation.utils.h5_utils import plot_ee_pose
-from visualization_msgs.msg import Marker
+from slap_manipulation.utils.h5_utils import plot_ee_pose, view_keyframe_imgs
 
-if __name__ == "__main__":
+
+@click.command()
+@click.option(
+    "--h5-file",
+    default="",
+    help="Absolute or relative path to h5 file to be introspected",
+)
+@click.option("--trial", default="", help="Trial name as a string which to introspect")
+def main(h5_file, trial):
+    if not h5_file:
+        print("No file path provided. Program will exit")
+        return
     rospy.init_node("h5_introspection")
     ros_pub = tf2_ros.TransformBroadcaster()
-    file = h5py.File("/home/priparashar/h5_test/stretch_testing/03-08_12-33-33.h5", "r")
-    plot_ee_pose(file, "0", ros_pub)
+    file = h5py.File(h5_file, "r")
+    view_keyframe_imgs(file, trial)
+    plot_ee_pose(file, trial, ros_pub)
+
+
+if __name__ == "__main__":
+    main()
