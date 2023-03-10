4f26cf5431568f17333d6ec3547e908701382713
ExhAustin <aswang@andrew.cmu.edu>
2023-03-10 11:54:05 -0800

    Replace test with new test

--- tests/hw_manual_test.py
+++ tests/hw_manual_test.py
@@ -2,60 +2,31 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
-import numpy as np
-import rospy
+import time
 
-from home_robot.motion.stretch import STRETCH_HOME_Q, HelloStretchKinematics
-from home_robot_hw.env.stretch_abstract_env import StretchEnv
+import numpy as np
+
+from home_robot_hw.stretch_client import StretchClient
 
 # Loose tolerances just to test that the robot moved reasonably
 POS_TOL = 0.1
 YAW_TOL = 0.2
 
 
-class StretchEnvImpl(StretchEnv):
-    def reset(self):
-        pass
-
-    def apply_action(self, action, info=None):
-        pass
-
-    def get_observation(self):
-        pass
-
-    @property
-    def episode_over(self):
-        pass
-
-    def get_episode_metrics(self):
-        pass
-
-
 if __name__ == "__main__":
     print("Initializing...")
-    rospy.init_node("hw_manual_test")
-    robot = StretchEnvImpl()
+    robot = StretchClient()
 
-    model = HelloStretchKinematics()
-
-    # Home robot
-    print("Homing robot...")
-    q = STRETCH_HOME_Q
-    robot.pretty_print(q)
-    robot.goto(q)
+    # Reset robot
+    print("Resetting robot...")
+    robot.reset()
 
     # Head movement
-    print("Testing robot joint movement...")
+    print("Testing robot head movement...")
+    robot.head.look_at_ee()
+    robot.head.look_front()
 
-    q = model.update_look_at_ee(q)
-    robot.goto(q, wait=True)
-    rospy.sleep(1.0)
-    q = model.update_look_front(q)
-    robot.goto(q, wait=True)
-
-    print(f"Confirm that the robot head has:")
-    print(f"\t1. Turned to face the gripper")
-    print(f"\t2. Turned back to face forwards")
+    print(f"Confirm that the robot head has moved accordingly.")
     input("(press enter to continue)")
 
     # Navigation
@@ -63,29 +34,21 @@ if __name__ == "__main__":
     robot.switch_to_navigation_mode()
 
     xyt_goal = [0.25, 0.25, -np.pi / 2]
-    robot.navigate_to(xyt_goal, blocking=True)
+    robot.nav.navigate_to(xyt_goal)
 
-    xyt_curr = robot.get_base_pose()
+    xyt_curr = robot.nav.get_base_pose()
     assert np.allclose(xyt_curr[:2], xyt_goal[:2], atol=POS_TOL)
     assert np.allclose(xyt_curr[2], xyt_goal[2], atol=YAW_TOL)
 
-    print(
-        f"Confirm that the robot moved to {xyt_goal} (moved forward-left, turned to face right)"
-    )
+    print(f"Confirm that the robot moved to {xyt_goal} (forward left, facing right)")
     input("(press enter to continue)")
 
     # Manipulation
     print("Testing robot manipulation...")
     robot.switch_to_manipulation_mode()
 
-    pos_diff_goal = np.array([0.0, -0.2, 0.2])
-    pos, quat = model.get_ee_pose(q)
-    q = model.manip_ik((pos + pos_diff_goal, quat))
-    robot.goto(q, wait=True)
+    pos_diff_goal = np.array([0.2, -0.2, 0.2])
+    robot.manip.goto_ee_pose(pos_diff_goal, relative=True)
 
-    print(
-        f"Confirm that the robot EE moved by {pos_diff_goal} (lift upwards and extend outwards by 20cm)"
-    )
+    print(f"Confirm that the robot EE moved by {pos_diff_goal} (X is forward, Z is up)")
     input("(press enter to continue)")
-
-    print("Test complete!")
