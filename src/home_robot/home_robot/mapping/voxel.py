219c1d4b9bd5d951ba8ab7911c5d82c9a93a2db2
Chris Paxton <cpaxton@fb.com>
2023-03-13 10:32:40 -0400

    Capture 3d data including camera intrinsics over time (#98)
    * add extra info to the 3d mapper tool
    
    * dump sparse voxel map data into a pickle
    
    * fix output
    
    * add original depth and rgb
    
    * Update src/home_robot/home_robot/mapping/voxel.py
    
    Co-authored-by: Austin Wang <austinswang@gmail.com>
    
    ---------
    
    Co-authored-by: Austin Wang <austinswang@gmail.com>

--- src/home_robot/home_robot/mapping/voxel.py
+++ src/home_robot/home_robot/mapping/voxel.py
@@ -2,6 +2,7 @@
 #
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
+import pickle
 from typing import Tuple
 
 import numpy as np
@@ -38,12 +39,12 @@ class SparseVoxelMap(object):
         self.feats = None
         self.observations = []
 
-    def add(self, camera_pose: np.ndarray, xyz: np.ndarray, feats: np.ndarray):
+    def add(self, camera_pose: np.ndarray, xyz: np.ndarray, feats: np.ndarray, **info):
         """Add this to our history of observations. Also update the current running map."""
         assert xyz.shape[-1] == 3
         assert feats.shape[-1] == self.feature_dim
         assert xyz.shape[0] == feats.shape[0]
-        self.observations.append((camera_pose, xyz, feats))
+        self.observations.append((camera_pose, xyz, feats, info))
         world_xyz = trimesh.transform_points(xyz, camera_pose)
 
         if self.xyz is None:
@@ -68,10 +69,30 @@ class SparseVoxelMap(object):
         else:
             return self.xyz.copy(), self.feats.copy()
 
+    def write_to_pickle(self, filename: str):
+        """Write out to a pickle file. This is a rough, quick-and-easy output for debugging, not intended to replace the scalable data writer in data_tools for bigger efforts."""
+        data = {}
+        data["poses"] = []
+        data["xyz"] = []
+        data["feats"] = []
+        for camera_pose, xyz, feats, info in self.observations:
+            # add it to pickle
+            data["poses"].append(camera_pose)
+            data["xyz"].append(xyz)
+            data["feats"].append(feats)
+            for k, v in info.items():
+                if k not in data:
+                    data[k] = []
+                data[k].append(v)
+        data["world_xyx"] = self.xyz
+        data["world_feats"] = self.feats
+        with open(filename, "wb") as f:
+            pickle.dump(data, f)
+
     def recompute_map(self):
         """Recompute the entire map from scratch instead of doing incremental updates"""
         self.xyz, self.feats = None, None
-        for camera_pose, xyz, feats in self.observations:
+        for camera_pose, xyz, feats, _ in self.observations:
             world_xyz = trimesh.transform_points(xyz, camera_pose)
             if self.xyz is None:
                 self.xyz, self.feats = world_xyz, feats
