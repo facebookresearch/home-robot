20c938ef6dfab51eb29c3fc02f3f6fa71c07edc1
Priyam Parashar <zephirefaith@users.noreply.github.com>
2023-03-16 16:06:50 -0400

    Finishing SLAP data-flow (adds action prediction module) (#102)
    * fix return type mismatch
    
    * creating preprocessing functions for inference-time usage
    
    * created action prediction input
    
    * uncomment
    
    * docstring
    
    * type hints
    
    * make weights-path a CLI arg
    
    * better documentation; better arg names
    
    * better documentation; better arg names
    
    * todo-->done

--- projects/slap_manipulation/scripts/slap_inference.py
+++ projects/slap_manipulation/scripts/slap_inference.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict, Tuple
+
 import hydra
 import numpy as np
 import rospy
@@ -7,6 +9,9 @@ from slap_manipulation.policy.action_prediction_module import ActionPredictionMo
 from slap_manipulation.policy.interaction_prediction_module import (
     InteractionPredictionModule,
 )
+from slap_manipulation.utils.input_preprocessing import (
+    get_local_action_prediction_problem,
+)
 
 from home_robot.utils.point_cloud import show_point_cloud
 from home_robot_hw.env.stretch_manipulation_env import StretchManipulationEnv
@@ -15,27 +20,61 @@ from home_robot_hw.env.stretch_manipulation_env import StretchManipulationEnv
 STRETCH_GRIPPER_MAX = 0.6
 
 
-def create_apm_input(raw_data, p_i):
+def get_proprio(raw_data, time=0.0):
+    """create a proprio vector from gripper-width
+    proprio_vector: (gripper-action, gripper-width, time in task)
+        gripper-action: 1.0 if gripper is closing, 0.0 if gripper is opening
+        gripper-width: actual gripper width  TODO: normalize this
+        time: normalized time in task (-1.0 is start of task, 1.0 is the end)
+    """
+    proprio = raw_data["gripper_state"].astype(np.float64)
+    if proprio < 0.8 * STRETCH_GRIPPER_MAX:
+        proprio = np.array([1.0, proprio, time])
+    else:
+        proprio = np.array([0.0, proprio, time])
+
+    return proprio
+
+
+def create_action_prediction_input(
+    cfg,
+    raw_data: Dict[str, Any],
+    ipm_input_vector: Tuple,
+    p_i: np.ndarray,
+    time: float = 0.0,
+):
     """takes raw data from stretch_manipulation_env and converts it into input batch
     for Action Prediction Module by cropping around predicted p_i: interaction_point"""
-    raise NotImplementedError
+    feat = np.concatenate(ipm_input_vector[0], axis=-1)
+    xyz = ipm_input_vector[1]
+    cropped_feat, cropped_xyz, status = get_local_action_prediction_problem(
+        cfg, feat, xyz, p_i
+    )
+    if not status:
+        raise RuntimeError(
+            "Interaction Prediction Module predicted an interaction point with no tractable local problem around it"
+        )
+    proprio = get_proprio(raw_data, time=time)
+    return (cropped_feat, cropped_xyz, proprio)
 
 
-def create_ipm_input(
+def create_interaction_prediction_input(
     raw_data: dict, lang: list[str], filter_depth=False, debug=False, num_pts=8000
 ):
     """takes raw data from stretch_manipulation_env, and language command from user.
-    Converts it into input batch used in Interaction Prediction Module"""
+    Converts it into input batch used in Interaction Prediction Module
+    Return: obs_vector = ((rgb), xyz, proprio, lang)
+        obs_vector[0]: tuple of features per point in PCD; each element is expected to be Nxfeat_dim
+        obs_vector[1]: xyz coordinates of PCD points; N x 3
+        obs_vector[2]: proprioceptive state of robot; 3-dim vector: [gripper-state, gripper-width, time] # probably do not need time for IPM training
+        obs_vector[3]: language command; list of 1 string # should this be a list? only 1 string.
+    """
     input_vector = ()
     depth = raw_data["depth"]
     rgb = raw_data["rgb"].astype(np.float64)
     xyz = raw_data["xyz"].astype(np.float64)
     camera_pose = raw_data["camera_pose"]
-    proprio = raw_data["gripper_state"].astype(np.float64)
-    if proprio < 0.8 * STRETCH_GRIPPER_MAX:
-        proprio = np.array([1.0, proprio, -1.0])
-    else:
-        proprio = np.array([0.0, proprio, -1.0])
+    proprio = get_proprio(raw_data, time=-1.0)
 
     depth = depth.reshape(-1)
     rgb = rgb.reshape(-1, 3)
@@ -62,12 +101,12 @@ def create_ipm_input(
     # mean-center the point cloud
     xyz -= xyz.mean(axis=0)
 
-    if debug:
-        show_point_cloud(xyz, rgb / 255.0, orig=np.zeros(3))
-
     if np.any(rgb) > 1.0:
         rgb = rgb / 255.0
-    input_vector = ([rgb], xyz, proprio, lang)
+    if debug:
+        show_point_cloud(xyz, rgb, orig=np.zeros(3))
+
+    input_vector = ((rgb), xyz, proprio, lang)
     return input_vector
 
 
@@ -77,13 +116,13 @@ def main(cfg):
     # create the robot object
     robot = StretchManipulationEnv(init_cameras=True)
     # create IPM object
-    ipm_model = InteractionPredictionModule(dry_run=cfg.dry_run)
-    ipm_model.to(ipm_model.device)
+    interaction_predictor = InteractionPredictionModule(dry_run=cfg.dry_run)
+    interaction_predictor.to(interaction_predictor.device)
     # create APM object
-    apm_model = ActionPredictionModule(dry_run=cfg.dry_run)
+    action_predictor = ActionPredictionModule(dry_run=cfg.dry_run)
     # load model-weights
-    ipm_model.load_state_dict(torch.load(cfg.ipm_weights))
-    # apm_model.load_state_dict(cfg.apm_weights)
+    interaction_predictor.load_state_dict(torch.load(cfg.ipm_weights))
+    action_predictor.load_state_dict(cfg.apm_weights)
 
     print("Loaded models successfully")
     cmds = [
@@ -106,20 +145,33 @@ def main(cfg):
         # get from the robot: pcd=(xyz, rgb), gripper-state,
         # construct input vector from raw data
         raw_observations = robot.get_observation()
-        input_vector = create_ipm_input(
+        ipm_input_vector = create_interaction_prediction_input(
             raw_observations, input_cmd, filter_depth=True, debug=True
         )
         # run inference on sensor data for IPM
-        interaction_point = ipm_model.predict(*input_vector)
+        (
+            interaction_point,
+            interaction_scores,
+            ipm_feat,
+            input_down_pcd,
+        ) = interaction_predictor.predict(*ipm_input_vector)
         print(f"Interaction point is {interaction_point}")
         experiment_running = False
         # ask if ok to run APM inference
-        if cfg.execution.use_regressor:
-            for i in range(1):  # cfg.num_keypoints
+        if cfg.execution.predict_action:
+            current_time = [-1.0, 0.0, 1.0]
+            for i in range(cfg.num_keypoints):
                 # run APM inference on sensor
                 raw_observations = robot.get_observation()
-                input_vector = create_apm_input(raw_observations, interaction_point)
-                action = apm_model.predict(*input_vector)
+                apm_input_vector = create_action_prediction_input(
+                    cfg,
+                    raw_observations,
+                    ipm_input_vector,
+                    interaction_point,
+                    time=current_time[i],
+                )
+                apm_input_vector += (input_cmd,)
+                action = action_predictor.predict(*apm_input_vector)
                 # ask if ok to execute
                 res = input("Execute the output? (y/n)")
                 if res == "y":
