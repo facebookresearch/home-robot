65510afdeeed9eef18cfb7b79133c7a869319b66
ExhAustin <aswang@andrew.cmu.edu>
2023-03-22 12:52:14 -0700

    Fix style

--- src/home_robot_hw/home_robot_hw/ros/grasp_helper.py
+++ src/home_robot_hw/home_robot_hw/ros/grasp_helper.py
@@ -20,6 +20,7 @@ from home_robot_hw.ros.utils import (
     matrix_to_pose_msg,
     ros_pose_to_transform,
 )
+
 try:
     from home_robot_hw.srv import GraspRequest, GraspRequestResponse
 except ModuleNotFoundError:
