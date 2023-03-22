5775a1ec16b0377c34bb3eb7be4587d359711804
Chris Paxton <cpaxton@fb.com>
2023-03-22 17:13:15 -0400

    add actions for sampling based agent

--- src/home_robot/home_robot/agent/objectnav_agent/sampling_agent.py
+++ src/home_robot/home_robot/agent/objectnav_agent/sampling_agent.py
@@ -10,7 +10,9 @@ import torch
 from torch.nn import DataParallel
 
 import home_robot.utils.pose as pu
-from home_robot.core.abstract_agent import Agent
+
+# from home_robot.core.abstract_agent import Agent
+from home_robot.agent.objectnav_agent import ObjectNavAgent
 from home_robot.core.interfaces import DiscreteNavigationAction, Observations
 from home_robot.mapping.voxel import SparseVoxelMap
 from home_robot.navigation_planner.rrt import RRTPlanner
@@ -18,20 +20,32 @@ from home_robot.navigation_planner.rrt import RRTPlanner
 from .objectnav_agent_module import ObjectNavAgentModule
 
 
-class SamplingBasedObjectNavAgent(Agent):
+class SamplingBasedObjectNavAgent(ObjectNavAgent):
     """Simple object nav agent based on a 2D semantic map"""
 
     def __init__(self, config, device_id: int = 0):
-        self.max_steps = config.AGENT.max_steps
-        self.num_environments = config.NUM_ENVIRONMENTS
+        super(SamplingBasedObjectNavAgent, self).__init__(config, device_id)
         self.planner = RRTPlanner()
         self.voxel_map = SparseVoxelMap()
 
     def reset(self):
         """Clear information in the voxel map"""
+        self.reset_vectorized()
         self.voxel_map.reset()
         self.planner.reset()
+        self.episode_panorama_start_steps = self.panorama_start_steps
 
     def act(self, obs: Observations) -> Tuple[DiscreteNavigationAction, Dict[str, Any]]:
         """Use this action to move around in the world"""
-        pass
+
+        # 1 - Obs preprocessing
+        (
+            obs_preprocessed,
+            pose_delta,
+            object_goal_category,
+            recep_goal_category,
+            goal_name,
+            camera_pose,
+        ) = self._preprocess_obs(obs)
+
+        return None, None
