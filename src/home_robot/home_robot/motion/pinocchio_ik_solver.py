98b70bd8c6b2aafcf9e099f1730675046fe3d4ac
Chris Paxton <cpaxton@fb.com>
2023-03-27 16:46:32 -0400

    improve docstrings for pin ik solver

--- src/home_robot/home_robot/motion/pinocchio_ik_solver.py
+++ src/home_robot/home_robot/motion/pinocchio_ik_solver.py
@@ -88,7 +88,14 @@ class PinocchioIKSolver:
         num_attempts: int = 1,
         verbose: bool = False,
     ) -> Tuple[np.ndarray, bool]:
-        """given end-effector position and quaternion, return joint values"""
+        """given end-effector position and quaternion, return joint values.
+
+        Two parameters are currently unused and might be implemented in the future:
+            q_init: initial configuration for the optimization to start in; especially useful for
+                    arms with redundant degrees of freedom
+            num_attempts: start from multiple initial configs; included for compatibility with pb
+            max iterations: time budget in number of steps; included for compatibility with pb
+        """
         i = 0
         if q_init is None:
             q = self.q_neutral.copy()
