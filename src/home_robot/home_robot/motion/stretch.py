e0cd78366f0b40e951dcbed4e090c95c2b584505
Chris Paxton <cpaxton@fb.com>
2023-03-27 16:41:27 -0400

    remove some dead code

--- src/home_robot/home_robot/motion/stretch.py
+++ src/home_robot/home_robot/motion/stretch.py
@@ -44,9 +44,6 @@ STRETCH_PREGRASP_Q = np.array(
         0.6,  # lift
         0.01,  # arm
         0.0,  # gripper rpy
-        # 3.12,  # wrist roll
-        # -1.51,  # wrist pitch
-        # -1.57,  # wrist yaw
         0.0,  # wrist roll
         -1.51,  # wrist pitch
         1.57,  # wrist yaw
