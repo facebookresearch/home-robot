ae33d40f617dfed2fbab74a922b1c5f33f4d62e5
ExhAustin <aswang@andrew.cmu.edu>
2023-04-12 11:32:29 -0700

    Update docstring

--- src/home_robot_hw/home_robot_hw/nodes/simple_grasp_server.py
+++ src/home_robot_hw/home_robot_hw/nodes/simple_grasp_server.py
@@ -90,18 +90,14 @@ def _generate_grasp(xyz: np.ndarray, rz: float) -> np.ndarray:
 
 def inference():
     """
-    Predict 6-DoF grasp distribution for given model and input data
+    Predict 6-DoF grasp distribution for given point cloud with a heuristic
 
-    :param global_config: config.yaml from checkpoint directory
-    :param checkpoint_dir: checkpoint directory
-    :param input_paths: .png/.npz/.npy file paths that contain depth/pointcloud and optionally intrinsics/segmentation/rgb
-    :param K: Camera Matrix with intrinsics to convert depth to point cloud
-    :param local_regions: Crop 3D local regions around given segments.
-    :param skip_border_objects: When extracting local_regions, ignore segments at depth map boundary.
-    :param filter_grasps: Filter and assign grasp contacts according to segmap.
-    :param segmap_id: only return grasps from specified segmap_id.
-    :param z_range: crop point cloud at a minimum/maximum z distance from camera to filter out outlier points. Default: [0.2, 1.8] m
-    :param forward_passes: Number of forward passes to run on each point cloud. Default: 1
+    Heuristic is as follows:
+        1. Voxelize the point cloud
+        2. Extract the top 10% voxels with highest Z coordinates in world frame
+        3. Project the said voxels into a 2D occupancy map
+        4. Compute grasp scores for each voxel based on neighboring occupancies
+        5. Generate top-down grasps if score > threshold
     """
 
     in_base_frame = True  # produced grasps are in base frame
