e1443fa5139f8ba2b55655d22b10eff40c6ccc83
Sriram Yenamandra <yvsriram99@gmail.com>
2023-04-24 07:37:10 -0400

    [temp] empty habitat test

--- tests/home_robot_sim/test_habitat_env.py
+++ tests/home_robot_sim/test_habitat_env.py
@@ -1,11 +1,2 @@
-import pytest
-
-try:
-    import habitat
-except ImportError:
-    print("Warning: habitat not installed, skipping habitat tests")
-    pytest.skip(allow_module_level=True)
-
-
 def test_objectnav_env():
     pass
