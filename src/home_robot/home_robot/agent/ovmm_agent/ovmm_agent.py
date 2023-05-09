b72559730e3bfd9defb47d2b6aa46daf7ec2fda8
Mukul Khanna <mukul18khanna@gmail.com>
2023-05-10 00:10:47 +0530

    Towards heuristic place support (#165)
    * First commit towards modular place
    
    * Fixes grip bug during arm movement
    
    * Renames modular -> heuristic, removes hardcoded policy
    
    * Adds hardcoded policy logic back
    
    * Visualizes point of placement and adds gripper yaw logic
    
    * Updates reachability check & uses Stretch's predefined arm/gripper length constants
    
    * Removes unused variables
    
    * Removes habitat util dependency, adds one in home-robot
    
    * Adds HeuristicPlacePolicy class
    
    * Adds place policy init file

--- src/home_robot/home_robot/agent/ovmm_agent/ovmm_agent.py
+++ src/home_robot/home_robot/agent/ovmm_agent/ovmm_agent.py
@@ -7,6 +7,7 @@ import torch
 from home_robot.agent.objectnav_agent.objectnav_agent import ObjectNavAgent
 from home_robot.agent.ovmm_agent.ppo_agent import PPOAgent
 from home_robot.core.interfaces import DiscreteNavigationAction, Observations
+from home_robot.place_policy.heuristic_place_policy import HeuristicPlacePolicy
 
 
 class Skill(IntEnum):
@@ -28,6 +29,7 @@ class OpenVocabManipAgent(ObjectNavAgent):
         self.is_pick_done = None
         self.place_done = None
         self.gaze_agent = None
+        self.place_policy = HeuristicPlacePolicy(config, self.device)
         if config.AGENT.SKILLS.PICK.type == "gaze":
             self.gaze_agent = PPOAgent(
                 config,
@@ -196,6 +198,9 @@ class OpenVocabManipAgent(ObjectNavAgent):
                 return DiscreteNavigationAction.STOP, vis_inputs
             elif self.config.AGENT.SKILLS.PLACE.type == "hardcoded":
                 action = self._hardcoded_place()
+                return action, vis_inputs
+            elif self.config.AGENT.SKILLS.PLACE.type == "heuristic_debug":
+                action, vis_inputs = self.place_policy(obs, vis_inputs)
                 return action, vis_inputs
             else:
                 raise NotImplementedError
