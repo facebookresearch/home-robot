ebfc6c246a7404b3b1a61e1ee4a07af5c80d7351
Priyam Parashar <priyam8parashar@gmail.com>
2023-05-12 14:05:12 -0400

    drafting agent class

--- projects/slap_manipulation/scripts/language_conditioned_manip_agent.py
+++ projects/slap_manipulation/scripts/language_conditioned_manip_agent.py
@@ -6,7 +6,7 @@ from home_robot_hw.env.stretch_pick_and_place_env import StretchPickandPlaceEnv
 
 def main():
     TASK = "bring me a cup from the table"
-    rospydebug.init_node("eval_episode_lang_ovmm")
+    rospy.init_node("eval_episode_lang_ovmm")
 
     env = StretchPickandPlaceEnv()
     agent = LangAgent()
@@ -20,7 +20,9 @@ def main():
         t += 1
         print("STEP =", t)
         obs = env.get_observation()
-        action, info = agent.act(obs, TASK)
-        env.apply_action(action, info=info)
+        steps = agent.get_steps(TASK)
+        for step in steps:
+            action, info = agent.act(obs, TASK)
+            env.apply_action(action, info=info)
 
     print(env.get_episode_metrics())
--- projects/slap_manipulation/src/slap_manipulation/agents/language_ovmm_agent.py
+++ projects/slap_manipulation/src/slap_manipulation/agents/language_ovmm_agent.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from home_robot.agent.hierarchical.pick_and_place_agent import (
     PickAndPlaceAgent,
     SimpleTaskState,
@@ -8,20 +10,31 @@ from home_robot.core.interfaces import Action, DiscreteNavigationAction, Observa
 class LangAgent(PickAndPlaceAgent):
     def __init__(self, cfg):
         super().__init__(cfg)
+        self.steps = []
         self.state = SimpleTaskState.NOT_STARTED
+        self.current_step = ""
+
+    def get_steps(self, task: str) -> List[str]:
+        """takes in a task string and returns a list of steps to complete the task"""
+        raise NotImplementedError
+
+    def get_actions(self, current_step: str) -> List[str]:
+        """takes in current step in natural language and returns list of actions required to complete it"""
+        raise NotImplementedError
 
     def get_ovmm_action_for_current_step(self):
-        action = None
+        action = []
         info = {}
         if "locate" in self.current_step or "goto" in self.current_step:
             # figure out how to pass a navigation command with info
-            return action, info
+            info["skill_language"] = self.current_step
         if "pick" in self.current_step:
             # figure out how to  pass a pick_object action with info
-            return action, info
+            info["action_list"] = self.get_actions(self.current_step)
+        return action, info
 
     def is_done(self):
-        if len(self.steps == 0 and self.state != SimpleTaskState.NOT_STARTED):
+        if len(self.steps) == 0 and self.state != SimpleTaskState.NOT_STARTED:
             return True
         else:
             return False
