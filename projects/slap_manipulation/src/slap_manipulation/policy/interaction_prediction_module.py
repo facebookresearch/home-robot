fea0fc89cd6d9433e8d65071ab75e5e58e4fd98e
Priyam Parashar <priyam8parashar@gmail.com>
2023-05-25 13:53:18 -0400

    renaming and recfging for new feats/trials

--- projects/slap_manipulation/src/slap_manipulation/policy/interaction_prediction_module.py
+++ projects/slap_manipulation/src/slap_manipulation/policy/interaction_prediction_module.py
@@ -232,7 +232,7 @@ class InteractionPredictionModule(torch.nn.Module):
         self._query_radius = 0.2
         self._max_neighbor = 64  # default from pointnet2 code
         # for mixing proprio and images
-        self.proprio_dim_size = 3
+        self.proprio_dim_size = 8
         self.im_channels = 64
         self.use_proprio = use_proprio
 
@@ -249,7 +249,7 @@ class InteractionPredictionModule(torch.nn.Module):
             0.5 * self._query_radius,
             128,
             self.device,
-            MLP([6, 64, 128], batch_norm=False),
+            MLP([7, 64, 128], batch_norm=False),
         )
         self.sa2_module = SAModule(
             # 0.1, 64, self.device, MLP([128 + 3, 128, 256, 512])
@@ -731,8 +731,10 @@ class InteractionPredictionModule(torch.nn.Module):
             optimizer.zero_grad()
             rgb = batch["rgb"][0]  # N x 3
             xyz = batch["xyz"][0]  # N x 3
+            feat = batch["feat"][0]
             down_xyz = batch["xyz_downsampled"][0]
             down_rgb = batch["rgb_downsampled"][0]
+            down_feat = batch["feat_downsampled"][0]
             lang = batch["cmd"]  # list of 1
             if self.use_proprio:
                 proprio = batch["proprio"][0]
@@ -745,11 +747,6 @@ class InteractionPredictionModule(torch.nn.Module):
             ee_keyframe = batch["ee_keyframe_pos"][0]
             ee_keyframe_rot = batch["ee_keyframe_ori"][0]
 
-            # Visualize
-            # show_point_cloud(
-            #        down_xyz.detach().cpu().numpy(),
-            #        down_rgb.detach().cpu().numpy(),
-            #        orig=np.zeros(3))
             if debug:
                 show_point_cloud_with_keypt_and_closest_pt(
                     down_xyz.detach().cpu().numpy(),
@@ -758,6 +755,10 @@ class InteractionPredictionModule(torch.nn.Module):
                     ee_keyframe_rot.detach().cpu().numpy().reshape(3, 3),
                     target_pos.detach().cpu().numpy().reshape(3, 1),
                 )
+
+            # combine rgb and feats
+            rgb = torch.cat([rgb, feat], dim=-1)
+            down_rgb = torch.cat([down_rgb, down_feat], dim=-1)
 
             # predict the closest centroid
             classification_probs, _, _ = self.forward(
@@ -969,7 +970,7 @@ def main(cfg):
         train_list = train_test_split["train"]
     else:
         train_test_split = None
-        train_list, valid_list, test_list = None, None, None
+        train_list, valid_list, test_list = [], [], []
     # Set up data augmentation
     # This is a warning for you - if things are not going well
     if cfg.data_augmentation:
@@ -977,7 +978,7 @@ def main(cfg):
     else:
         print("-> NOT using data augmentation.")
     # Set up data loaders
-    if cfg.source == "robopen":
+    if cfg.source in ["robopen", "stretch"]:
         # Get the robopebn dataset
         Dataset = RobotDataset
         train_dataset = RobotDataset(
@@ -991,6 +992,8 @@ def main(cfg):
             color_jitter=cfg.color_jitter,
             template=cfg.template,
             dr_factor=5,
+            autoregressive=True,
+            multi_step=True,
         )
         valid_dataset = RobotDataset(
             cfg.datadir,
@@ -1000,7 +1003,8 @@ def main(cfg):
             keypoint_range=[0, 1, 2],
             color_jitter=False,
             template=cfg.template,
-            # template="**/*.h5",
+            autoregressive=True,
+            multi_step=True,
         )
         test_dataset = RobotDataset(
             cfg.datadir,
@@ -1010,7 +1014,8 @@ def main(cfg):
             keypoint_range=[0, 1, 2],
             color_jitter=False,
             template=cfg.template,
-            # template="**/*.h5",
+            autoregressive=True,
+            multi_step=True,
         )
     else:
         # get rlbench dataset
