b09c2bf062497697f4314ed4ccdb653e7f30c9be
Priyam Parashar <zephirefaith@users.noreply.github.com>
2023-05-31 19:08:15 -0400

    debug print-and-stop

--- src/home_robot/home_robot/navigation_planner/discrete_planner.py
+++ src/home_robot/home_robot/navigation_planner/discrete_planner.py
@@ -382,6 +382,7 @@ class DiscretePlanner:
                 action = ContinuousNavigationAction([0, 0, -relative_angle_goal])
             else:
                 action = DiscreteNavigationAction.STOP
+                input("Look at RViz, verify current scene has all goals in it. Press enter to continue")
                 print("!!! DONE !!!")
 
         self.last_action = action
