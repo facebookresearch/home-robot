1e1c6e92da8e806c03aa3a79bab010ac14f55825
Chris Paxton <paxton.chris@live.com>
2023-06-01 09:00:48 -0700

    change the launch files to make sure clock is published - avoid time sync issues

--- src/home_robot_hw/home_robot_hw/nodes/clock.py
+++ src/home_robot_hw/home_robot_hw/nodes/clock.py
@@ -7,15 +7,20 @@ from rosgraph_msgs.msg import Clock
 
 
 class MasterClock:
-    def __init__(self, spin_rate=10000):
+    def __init__(self, spin_rate=10000, wall_time=True):
         self.spin_rate = spin_rate
+        self.wall_time = wall_time
         self._dt = 1.0 / self.spin_rate
         self._pub = rospy.Publisher("/clock", Clock, queue_size=1)
         self._t = 0
 
     def spin(self):
+        """Spin in a loop while publishing time signal. Because this is the "master" clock used for ROS time thoughout HomeRobot, it must not use ROS sleeps!"""
         while not rospy.is_shutdown():
-            self._pub.publish(rospy.Time(self._t))
+            if self.wall_time:
+                self._pub.publish(rospy.Time(time.time()))
+            else:
+                self._pub.publish(rospy.Time(self._t))
             time.sleep(self._dt)
             self._t += self._dt
 
