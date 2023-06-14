88e104ce352ef5f013208a4cb74b6460907b6fff
Theo Gervet <theop123@meta.com>
2023-06-14 13:49:33 -0700

    Debug

--- src/home_robot/home_robot/navigation_planner/discrete_planner.py
+++ src/home_robot/home_robot/navigation_planner/discrete_planner.py
@@ -432,8 +432,6 @@ class DiscretePlanner:
         # Create inverse map of obstacles - this is territory we assume is traversible
         # Traversible is now the map
         traversible = 1 - dilated_obstacles
-        if self.collision_map.sum() > 0:
-            breakpoint()
         traversible[self.collision_map[gx1:gx2, gy1:gy2][x1:x2, y1:y2] == 1] = 0
         traversible[self.visited_map[gx1:gx2, gy1:gy2][x1:x2, y1:y2] == 1] = 1
         agent_rad = self.agent_cell_radius
