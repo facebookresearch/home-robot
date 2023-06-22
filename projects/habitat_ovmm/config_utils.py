56d11cd566c57f8c7662da9ba53c88b7ab8aea0a
Chris Paxton <cpaxton@fb.com>
2023-06-22 12:59:06 -0400

    Update headers to add copyright notice (#248)

--- projects/habitat_ovmm/config_utils.py
+++ projects/habitat_ovmm/config_utils.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 from typing import Optional, Tuple
 
 from habitat_baselines.config.default import _BASELINES_CFG_DIR
--- src/home_robot/home_robot/utils/point_cloud_torch.py
+++ src/home_robot/home_robot/utils/point_cloud_torch.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 """
 This file contains versions of the helpers in point_cloud.py that use pytorch directly (rather than numpy),
 to allow operations to be done on the GPU for speed.
