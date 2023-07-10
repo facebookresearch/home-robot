5665a97658bd70e27ee5e695e01bae2158dce593
Priyam Parashar <priyam8parashar@gmail.com>
2023-07-10 12:10:18 -0400

    licenses

--- projects/slap_manipulation/src/slap_manipulation/policy/peract.py
+++ projects/slap_manipulation/src/slap_manipulation/policy/peract.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
 # Dependency taken from PerAct for easy integration
 # License: https://github.com/peract/peract/blob/main/LICENSE
 import copy
