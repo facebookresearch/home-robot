39dd656ded7b24ef381ec8484d455f6f8b86cd55
theop123 <theophile.gervet@gmail.com>
2023-07-10 16:44:20 -0700

    Improvements

--- projects/offline_mapping/build_map.py
+++ projects/offline_mapping/build_map.py
@@ -120,7 +120,7 @@ def main(input_trajectory_path, output_visualization_path):
     ):
         with open(path, "rb") as f:
             observations.append(pickle.load(f))
-    observations = observations[:1]  # TODO Debug
+    observations = observations[:50]  # TODO Debug
 
     # Predict semantic segmentation
     categories = [
