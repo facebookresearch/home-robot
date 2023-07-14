ea906b485be836192d16098de490d2f81658e205
Mukul Khanna <mukul18khanna@gmail.com>
2023-07-14 17:19:15 -0400

    Support for extracting target and landmark from ChatGPT directly

--- projects/habitat_languagenav/scripts/goat/create_langnav_dataset_subset_with_gpt_responses.py
+++ projects/habitat_languagenav/scripts/goat/create_langnav_dataset_subset_with_gpt_responses.py
@@ -5,14 +5,14 @@ import json
 import os
 import random
 
-from gpt.run import generate_gpt_response
+from gpt.run_target_landmark_extraction import generate_gpt_response
 from tqdm import tqdm
 
 N = 100  # number of episodes to make subset from
 random.seed(100)
 
 langnav_episodes_file = (
-    "data/datasets/languagenav/hm3d/v2/val/content/vLpv2VX547B.json.gz"
+    "data/datasets/languagenav/hm3d/v0/val/content/vLpv2VX547B.json.gz"
 )
 
 # open gzip file and load json
@@ -21,22 +21,30 @@ with gzip.open(langnav_episodes_file, "rt") as f:
 
 langnav_episodes = langnav_episodes_dset["episodes"]
 random_episodes = random.sample(langnav_episodes, N)
-
 for ep in tqdm(random_episodes):
-    instruction = ep["instructions"][0].replace("Instruction: ", "# ")
+    # instruction = ep["instructions"][0].replace("Instruction: ", "# ")
+    instruction = ep["instructions"][0]
 
     try:
-        gpt_response = generate_gpt_response(ep["instructions"][0])
+        gpt_response = generate_gpt_response(instruction)
+        target, landmarks = gpt_response.split("\n")
+        target = target.replace("Target: ", "")
+        landmarks = landmarks.replace("Landmarks: ", "")
+        landmarks = landmarks.split(", ")
     except Exception as e:
         # to capture occassional RateLimitError
         print(e)
         print("Saving episodes with responses generated so far...")
         break
 
+    if len(landmarks) == 1 and landmarks[0] == "None":
+        landmarks = []
+
     ep["llm_response"] = {
-        "landmark": None,
+        "target": target,
+        "landmark": landmarks,
         "room_name": None,
-        "full_response": gpt_response,
+        "full_response": ep["llm_response"]["full_response"],
     }
 
 # create a copy of langnav_episode_dset
@@ -47,7 +55,7 @@ langnav_episodes_dset_subset["episodes"] = [
 
 # save to file
 langnav_episodes_file_subset = (
-    "data/datasets/languagenav/hm3d/v2/minival/content/vLpv2VX547B.json.gz"
+    "data/datasets/languagenav/hm3d/v0/val/content/vLpv2VX547B.json.gz"
 )
 os.makedirs(os.path.dirname(langnav_episodes_file_subset), exist_ok=True)
 with gzip.open(langnav_episodes_file_subset, "wt") as f:
