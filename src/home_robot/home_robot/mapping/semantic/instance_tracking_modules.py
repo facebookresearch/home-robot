7905179f0ad95ef1be6be81fb38b006b5a79ddfd
Sriram Yenamandra <yvsriram99@gmail.com>
2023-07-17 09:20:12 -0400

    add category name to viz for objectnav, instance_tracking in offline mapping

--- src/home_robot/home_robot/mapping/semantic/instance_tracking_modules.py
+++ src/home_robot/home_robot/mapping/semantic/instance_tracking_modules.py
@@ -135,8 +135,8 @@ class InstanceMemory:
 
             os.makedirs(f"instances/{global_instance_id}", exist_ok=True)
             cv2.imwrite(
-                f"instances/{global_instance_id}/{self.timesteps[env_id]}_{local_instance_id}.png",
-                instance_view.cropped_image,
+                f"instances/{global_instance_id}/{self.timesteps[env_id]}_{local_instance_id}_cat_{instance_view.category_id}.png",
+                instance_view.cropped_image[:, :, ::-1],
             )
             print(
                 "mapping local instance id",
