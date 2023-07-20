2ba714b9837d6fa36e199cfafd2913e49b201fe4
theop123 <theophile.gervet@gmail.com>
2023-07-20 13:18:45 -0700

    Swap Detic for Maskrcnn

--- src/home_robot/home_robot/perception/detection/detic/detic_perception.py
+++ src/home_robot/home_robot/perception/detection/detic/detic_perception.py
@@ -20,6 +20,7 @@ from detectron2.utils.visualizer import ColorMode, Visualizer
 
 from home_robot.core.abstract_perception import PerceptionModule
 from home_robot.core.interfaces import Observations
+from home_robot.perception.detection.utils import filter_depth, overlay_masks
 
 sys.path.insert(
     0, str(Path(__file__).resolve().parent / "Detic/third_party/CenterNet2/")
@@ -61,42 +62,6 @@ def get_clip_embeddings(vocabulary, prompt="a "):
     texts = [prompt + x for x in vocabulary]
     emb = text_encoder(texts).detach().permute(1, 0).contiguous().cpu()
     return emb
-
-
-def overlay_masks(
-    masks: np.ndarray, class_idcs: np.ndarray, shape: Tuple[int, int]
-) -> np.ndarray:
-    """Overlays the masks of objects
-    Determines the order of masks based on mask size
-    """
-    mask_sizes = [np.sum(mask) for mask in masks]
-    sorted_mask_idcs = np.argsort(mask_sizes)
-
-    semantic_mask = np.zeros(shape)
-    instance_mask = -np.ones(shape)
-    for i_mask in sorted_mask_idcs[::-1]:  # largest to smallest
-        semantic_mask[masks[i_mask].astype(bool)] = class_idcs[i_mask]
-        instance_mask[masks[i_mask].astype(bool)] = i_mask
-
-    return semantic_mask, instance_mask
-
-
-def filter_depth(
-    mask: np.ndarray, depth: np.ndarray, depth_threshold: Optional[float] = None
-) -> np.ndarray:
-    md = np.median(depth[mask == 1])  # median depth
-    if md == 0:
-        # Remove mask if more than half of points has invalid depth
-        filter_mask = np.ones_like(mask, dtype=bool)
-    elif depth_threshold is not None:
-        # Restrict objects to 1m depth
-        filter_mask = (depth >= md + depth_threshold) | (depth <= md - depth_threshold)
-    else:
-        filter_mask = np.zeros_like(mask, dtype=bool)
-    mask_out = mask.copy()
-    mask_out[filter_mask] = 0.0
-
-    return mask_out
 
 
 class DeticPerception(PerceptionModule):
--- src/home_robot/home_robot/perception/detection/maskrcnn/maskrcnn_perception.py
+++ src/home_robot/home_robot/perception/detection/maskrcnn/maskrcnn_perception.py
@@ -23,44 +23,9 @@ from detectron2.utils.visualizer import ColorMode, VisImage, Visualizer
 
 from home_robot.core.abstract_perception import PerceptionModule
 from home_robot.core.interfaces import Observations
+from home_robot.perception.detection.utils import filter_depth, overlay_masks
 
 from .coco_categories import coco_categories, coco_categories_mapping
-
-
-def overlay_masks(
-    masks: np.ndarray, class_idcs: np.ndarray, shape: Tuple[int, int]
-) -> np.ndarray:
-    """Overlays the masks of objects
-    Determines the order of masks based on mask size
-    """
-    mask_sizes = [np.sum(mask) for mask in masks]
-    sorted_mask_idcs = np.argsort(mask_sizes)
-
-    semantic_mask = np.zeros(shape)
-    instance_mask = -np.ones(shape)
-    for i_mask in sorted_mask_idcs[::-1]:  # largest to smallest
-        semantic_mask[masks[i_mask].astype(bool)] = class_idcs[i_mask]
-        instance_mask[masks[i_mask].astype(bool)] = i_mask
-
-    return semantic_mask, instance_mask
-
-
-def filter_depth(
-    mask: np.ndarray, depth: np.ndarray, depth_threshold: Optional[float] = None
-) -> np.ndarray:
-    md = np.median(depth[mask == 1])  # median depth
-    if md == 0:
-        # Remove mask if more than half of points has invalid depth
-        filter_mask = np.ones_like(mask, dtype=bool)
-    elif depth_threshold is not None:
-        # Restrict objects to 1m depth
-        filter_mask = (depth >= md + depth_threshold) | (depth <= md - depth_threshold)
-    else:
-        filter_mask = np.zeros_like(mask, dtype=bool)
-    mask_out = mask.copy()
-    mask_out[filter_mask] = 0.0
-
-    return mask_out
 
 
 class MaskRCNNPerception(PerceptionModule):
@@ -133,6 +98,7 @@ class MaskRCNNPerception(PerceptionModule):
         # Sort instances by mask size
         masks = pred["instances"].pred_masks.cpu().numpy()
         class_idcs = pred["instances"].pred_classes.cpu().numpy()
+        class_idcs = np.array([coco_categories_mapping[idx] for idx in class_idcs])
         scores = pred["instances"].scores.cpu().numpy()
 
         if depth_threshold is not None and depth is not None:
