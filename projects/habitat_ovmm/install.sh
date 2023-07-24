f97f76b6d8eec610f2f73d6435b0591bf83c14d8
Chris Paxton <cpaxton@jhu.edu>
2023-07-24 19:12:28 -0400

    FIx the install script

--- projects/habitat_ovmm/install.sh
+++ projects/habitat_ovmm/install.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
 
 # Download the scenes
 cd $HOME_ROBOT_ROOT
+git submodule update --init --recursive
 git submodule update --init data/hssd-hab
 
 # Download the objects and metadata
@@ -25,6 +31,7 @@ cd -
 
 mkdir -p $HOME_ROBOT_ROOT/data/robots/hab_stretch
 cd $HOME_ROBOT_ROOT/data/robots/hab_stretch
+echo $HOME_ROBOT_ROOT/data/robots/hab_stretch
 
 wget http://dl.fbaipublicfiles.com/habitat/robots/hab_stretch_v1.0.zip
 unzip hab_stretch_v1.0.zip
