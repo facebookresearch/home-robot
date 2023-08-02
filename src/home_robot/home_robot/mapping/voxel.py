3604e5a8ad162b7f4b9beefcd34aeb03799b2281
Chris Paxton <cpaxton@jhu.edu>
2023-08-02 16:52:02 -0400

    fixing voxel map + making sure it supports more sizes; updating code

--- src/home_robot/home_robot/mapping/voxel.py
+++ src/home_robot/home_robot/mapping/voxel.py
@@ -6,6 +6,7 @@ import pickle
 from typing import Tuple
 
 import numpy as np
+import open3d as o3d
 import trimesh
 
 from home_robot.utils.point_cloud import numpy_to_pcd, pcd_to_numpy, show_point_cloud
@@ -25,8 +26,7 @@ def combine_point_clouds(
     else:
         pc_rgb = np.concatenate([pc_rgb, rgb], axis=0)
         pc_xyz = np.concatenate([pc_xyz, xyz], axis=0)
-    pcd = numpy_to_pcd(pc_xyz, pc_rgb).voxel_down_sample(voxel_size=sparse_voxel_size)
-    return pcd_to_numpy(pcd)
+    return numpy_to_pcd(pc_xyz, pc_rgb).voxel_down_sample(voxel_size=sparse_voxel_size)
 
 
 class SparseVoxelMap(object):
@@ -44,18 +44,24 @@ class SparseVoxelMap(object):
         assert xyz.shape[-1] == 3
         assert feats.shape[-1] == self.feature_dim
         assert xyz.shape[0] == feats.shape[0]
+
+        if len(xyz.shape) > 2:
+            xyz = xyz.reshape(-1, 3)
+            feats = feats.reshape(-1, 3)
+
         self.observations.append((camera_pose, xyz, feats, info))
         world_xyz = trimesh.transform_points(xyz, camera_pose)
 
         # Combine point clouds by adding in the current view to the previous ones and
         # voxelizing.
-        self.xyz, self.feats = combine_point_clouds(
+        self._pcd = combine_point_clouds(
             self.xyz,
             self.feats,
             world_xyz,
             feats,
             sparse_voxel_size=self.resolution,
         )
+        self.xyz, self.feats = pcd_to_numpy(self._pcd)
 
     def get_data(self, in_place: bool = True) -> Tuple[np.ndarray, np.ndarray]:
         """Return the current point cloud and features; optionally copying."""
@@ -90,12 +96,17 @@ class SparseVoxelMap(object):
         for camera_pose, xyz, feats, _ in self.observations:
             world_xyz = trimesh.transform_points(xyz, camera_pose)
             if self.xyz is None:
-                self.xyz, self.feats = world_xyz, feats
+                pc_xyz, pc_rgb = world_xyz, feats
             else:
                 pc_rgb = np.concatenate([self.feats, feats], axis=0)
                 pc_xyz = np.concatenate([self.xyz, world_xyz], axis=0)
-        pcd = numpy_to_pcd(pc_xyz, pc_rgb).voxel_down_sample(voxel_size=self.resolution)
-        self.xyz, self.feats = pcd_to_numpy(pcd)
+        self._pcd = numpy_to_pcd(pc_xyz, pc_rgb).voxel_down_sample(
+            voxel_size=self.resolution
+        )
+        self.xyz, self.feats = pcd_to_numpy(self._pcd)
+
+    def get_kd_tree(self):
+        return o3d.geometry.KDTreeFlann(self._pcd)
 
     def reset(self) -> None:
         """Clear out the entire voxel map."""
