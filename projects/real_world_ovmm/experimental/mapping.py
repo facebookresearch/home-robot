51c8195daa100f2951c02020fd06c432252ffb6d
Chris Paxton <cpaxton@jhu.edu>
2023-08-02 17:39:35 -0400

    open3d code to scale up voxels and create a robot base mesh

--- projects/real_world_ovmm/experimental/mapping.py
+++ projects/real_world_ovmm/experimental/mapping.py
@@ -14,13 +14,14 @@
 import click
 import matplotlib.pyplot as plt
 import numpy as np
+import open3d as o3d
 import rospy
 from fastsam import FastSAM, FastSAMPrompt
 
 import home_robot
 import home_robot_hw
 from home_robot.mapping.voxel import SparseVoxelMap
-from home_robot.utils.point_cloud import show_point_cloud
+from home_robot.utils.point_cloud import create_visualization_geometries, numpy_to_pcd
 from home_robot_hw.remote.api import StretchClient
 from home_robot_hw.utils.config import load_config
 
@@ -52,23 +53,27 @@ def try_sam(image, model, debug=True):
     return everything_results
 
 
+def show_map(
+    xyz,
+    rgb,
+    robot_model: np.ndarray,
+    robot_pose: np.ndarray,
+    orig: np.ndarray = None,
+    R: np.ndarray = None,
+    save: str = None,
+    grasps: list = None,
+    size: float = 0.1,
+) -> None:
+
+    pcd = numpy_to_pcd(xyz, rgb)
+    geoms = create_visualization_geometries(pcd=pcd, orig=orig, size=size)
+    geoms += [robot_model]
+    o3d.visualization.draw_geometries(geoms)
+
+
 @click.command()
-@click.option("--test-pick", default=False, is_flag=True)
-@click.option("--test-gaze", default=False, is_flag=True)
-@click.option("--test-place", default=False, is_flag=True)
-@click.option("--skip-gaze", default=True, is_flag=True)
 @click.option("--reset-nav", default=False, is_flag=True)
-@click.option("--dry-run", default=False, is_flag=True)
-@click.option("--pick-object", default="cup")
-@click.option("--start-recep", default="table")
-@click.option("--goal-recep", default="chair")
 @click.option("--sam-weights", default="./weights/FastSAM-x.pt")
-@click.option(
-    "--cat-map-file", default="projects/real_world_ovmm/configs/example_cat_map.json"
-)
-@click.option("--max-num-steps", default=200)
-@click.option("--visualize-maps", default=False, is_flag=True)
-@click.option("--visualize-grasping", default=False, is_flag=True)
 @click.option(
     "--debug",
     default=False,
@@ -76,26 +81,25 @@ def try_sam(image, model, debug=True):
     help="Add pauses for debugging manipulation behavior.",
 )
 def main(
-    test_pick=False,
+    debug=False,
     reset_nav=False,
-    pick_object="cup",
-    start_recep="table",
-    goal_recep="chair",
-    dry_run=False,
-    visualize_maps=False,
-    visualize_grasping=False,
-    test_place=False,
-    cat_map_file=None,
-    max_num_steps=200,
     sam_weights=None,
-    **kwargs,
+    voxel_size=0.1,
 ):
     print("- Creating robot client")
     robot = StretchClient()
     print("- Loading SAM weights")
     sam = load_sam_model(sam_weights)
-    print("- Creating voxel map")
-    voxel_map = SparseVoxelMap(resolution=0.01)
+    print("- Creating voxel map with resolution =", voxel_size, "m")
+    voxel_map = SparseVoxelMap(resolution=voxel_size)
+
+    # Placeholder robot mesh
+    print("- create robot mesh geometry for Stretch")
+    # robot_mesh_data = o3d.geometry.TriangleMesh.create_sphere(radius=0.3)
+    # robot_base = o3d.geometry.TriangleMesh.create_box(width=0.33, height=0.15, depth=0.36)
+    robot_base = o3d.geometry.TriangleMesh.create_box(
+        width=0.33, height=0.36, depth=0.15
+    )
 
     t = 0
     while not rospy.is_shutdown():
@@ -107,15 +111,16 @@ def main(
             compute_xyz=True,
         )
         # Get the camera pose and make sure this works properly
-        camera_pose = self.robot.head.get_pose(rotated=False)
+        camera_pose = robot.head.get_pose(rotated=False)
+        robot_pose = robot.nav.get_base_pose()
 
         # run a segmenter
-        res = try_sam(obs.rgb, sam, debug=True)
+        res = try_sam(rgb, sam, debug=True)
 
         # Update the voxel map
         voxel_map.add(camera_pose, xyz, rgb)
         pc_xyz, pc_rgb = voxel_map.get_data()
-        show_point_cloud(pc_xyz, pc_rgb / 255, orig=np.zeros(3))
+        show_map(pc_xyz, pc_rgb / 255, robot_base, robot_pose, orig=np.zeros(3))
         breakpoint()
 
 
