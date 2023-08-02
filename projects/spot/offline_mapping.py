63b1dae45adddccc308f96446aa73cd883f4550b
theop123 <theophile.gervet@gmail.com>
2023-08-02 11:47:47 -0700

    Test language grounding

--- projects/spot/offline_mapping.py
+++ projects/spot/offline_mapping.py
@@ -261,8 +261,8 @@ def text_to_image(
 
 
 record_instance_ids = True
-save_map_and_instances = False
-load_map_and_instances = True
+save_map_and_instances = True
+load_map_and_instances = False
 ground_image_in_memory = False
 ground_language_in_memory = True
 
