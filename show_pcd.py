f0d16b73c10ca174e0343801e5d8958b6a25aa42
Chris Paxton <cpaxton@jhu.edu>
2023-08-08 18:08:07 -0400

    update code

--- show_pcd.py
+++ show_pcd.py
@@ -1,3 +1,4 @@
+import click
 import numpy as np
 import trimesh
 
@@ -12,63 +13,72 @@ from home_robot.utils.point_cloud import show_point_cloud
 python projects/habitat_ovmm/eval_baselines_agent.py --baseline_config_path projects/habitat_ovmm/configs/agent/pick_skill_only.yaml --env_config_path projects/habitat_ovmm/configs/env/hssd_demo_gt.yaml habitat.task.pick_init=True habitat.task.episode_init=False habitat.environment.max_episode_steps=10  habitat.task.actions.arm_action.grasp_thresh_dist=0.1 habitat.dataset.episode_ids="[0]"
 """
 
-# Debug pointclouds
-show_pcds = False
 
-data = np.load("test.npz")
-all_xyz = data["all_xyz"]
-xyz = data["xyz"]
-rgb = data["rgb"]
-pt = data["pt"]
+def main(show_pcds=True):
+    # Debug pointclouds
 
-all_xyz = all_xyz.reshape(-1, 3)
-all_rgb = rgb.reshape(-1, 3)
+    data = np.load("test-sriram.npz")
+    all_xyz = data["all_xyz"]
+    # Object XYZ
+    # xyz = data["xyz"]
+    rgb = data["rgb"]
+    pt = data["pt"]
 
-print("Show observations from habitat")
-if show_pcds:
-    show_point_cloud(all_xyz, rgb / 255, orig=np.zeros(3))
-rot = tra.euler_matrix(0, 0, np.pi)
-all_xyz = trimesh.transform_points(all_xyz, rot)
+    all_xyz = all_xyz.reshape(-1, 3)
+    all_rgb = rgb.reshape(-1, 3)
 
-# Compute it in the correct frame
-print("Show corrected point cloud (from the previous observations from habitat)")
-pt = trimesh.transform_points(pt[None], rot)[0]
-if show_pcds:
-    show_point_cloud(all_xyz, rgb / 255, orig=pt)
-    # show_point_cloud(xyz, rgb / 255, orig=pt)
+    print("Show observations from habitat")
+    if show_pcds:
+        show_point_cloud(all_xyz, all_rgb / 255, orig=np.zeros(3))
+    rot = tra.euler_matrix(0, 0, np.pi)
+    all_xyz = trimesh.transform_points(all_xyz, rot)
 
-client = PbClient(visualize=True)
-red_block = PbObject(
-    "red_block", "./assets/red_block.urdf", start_pos=pt, client=client.id
-)
-PLANNER_STRETCH_URDF = "assets/hab_stretch/urdf/planner_calibrated.urdf"
-MANIP_STRETCH_URDF = "assets/hab_stretch/urdf/stretch_manip_mode.urdf"
-# Load a robot model here
-robot = client.add_articulated_object("robot", MANIP_STRETCH_URDF)
+    # Compute it in the correct frame
+    print("Show corrected point cloud (from the previous observations from habitat)")
+    pt = trimesh.transform_points(pt[None], rot)[0]
+    if show_pcds:
+        show_point_cloud(all_xyz, all_rgb / 255, orig=pt)
+        # show_point_cloud(xyz, rgb / 255, orig=pt)
 
-# Inverse kinematics
-grasp_orientation = tra.quaternion_from_euler(0, np.pi, 0)
+    client = PbClient(visualize=True)
+    red_block = PbObject(
+        "red_block", "./assets/red_block.urdf", start_pos=pt, client=client.id
+    )
+    print("Created object to track grasp pt:", red_block)
+    # PLANNER_STRETCH_URDF = "assets/hab_stretch/urdf/planner_calibrated.urdf"
+    MANIP_STRETCH_URDF = "assets/hab_stretch/urdf/stretch_manip_mode.urdf"
+    # Load a robot model here
+    robot = client.add_articulated_object("robot", MANIP_STRETCH_URDF)
 
-model = HelloStretchKinematics()
+    # Inverse kinematics
+    grasp_orientation = tra.quaternion_from_euler(0, np.pi, 0)
 
-print("Target point to grasp:", pt)
-print("Raising the point by the size of the stretch gripper before doing IK...")
-pt[2] += STRETCH_STANDOFF_DISTANCE
-print("Target point to grasp:", pt)
+    model = HelloStretchKinematics()
 
-cfg, res, info = model.manip_ik((pt, grasp_orientation))
-print("cfg =", cfg)
-print("res =", res)
-print("inf =", info)
+    print("Target point to grasp:", pt)
+    print("Raising the point by the size of the stretch gripper before doing IK...")
+    pt[2] += STRETCH_STANDOFF_DISTANCE
+    print("Target point to grasp:", pt)
 
-model.set_articulated_object_positions(robot, cfg)
+    cfg, res, info = model.manip_ik((pt, grasp_orientation))
+    print("cfg =", cfg)
+    print("res =", res)
+    print("inf =", info)
 
-print()
-print("After switching to manipulation mode, then you can move the robot like this:")
-print("base x motion:", cfg[0])
-print("lift:         ", cfg[1])
-print("arm extension:", cfg[2] + cfg[3] + cfg[4] + cfg[5])
-print("wrist yaw:    ", cfg[6])
-print("wrist pitch:  ", cfg[7])
-print("wrist roll:   ", cfg[8])
-input("press enter when done")
+    model.set_articulated_object_positions(robot, cfg)
+
+    print()
+    print(
+        "After switching to manipulation mode, then you can move the robot like this:"
+    )
+    print("base x motion:", cfg[0])
+    print("lift:         ", cfg[1])
+    print("arm extension:", cfg[2] + cfg[3] + cfg[4] + cfg[5])
+    print("wrist yaw:    ", cfg[6])
+    print("wrist pitch:  ", cfg[7])
+    print("wrist roll:   ", cfg[8])
+    input("press enter when done")
+
+
+if __name__ == "__main__":
+    main()
