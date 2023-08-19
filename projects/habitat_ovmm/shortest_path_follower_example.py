b35e151b3c56ac3e80e3d0b4fdcb1c5dd73a2bcd
Prasoon Varshney <pvarshne@andrew.cmu.edu>
2023-08-19 17:15:50 -0400

    Shortest path example script updates

--- projects/habitat_ovmm/shortest_path_follower_example.py
+++ projects/habitat_ovmm/shortest_path_follower_example.py
@@ -8,9 +8,12 @@ import argparse
 import os
 
 import magnum as mn
+import numpy as np
 from habitat.core.utils import try_cv2_import
 from habitat.sims.habitat_simulator.actions import HabitatSimActions
 from habitat.tasks.nav.shortest_path_follower import ShortestPathFollower
+from habitat.tasks.utils import cartesian_to_polar
+from habitat.utils.geometry_utils import quaternion_from_coeff, quaternion_rotate_vector
 from habitat.utils.visualizations import maps
 from habitat.utils.visualizations.utils import images_to_video
 from utils.config_utils import (
@@ -32,6 +35,14 @@ if not os.path.exists(IMAGE_DIR):
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
 os.environ["NUMEXPR_NUM_THREADS"] = "1"
 os.environ["MKL_NUM_THREADS"] = "1"
+
+# def _quat_to_xy_heading(quat):
+#     direction_vector = np.array([0, 0, -1])
+
+#     heading_vector = quaternion_rotate_vector(quat, direction_vector)
+
+#     phi = cartesian_to_polar(-heading_vector[2], heading_vector[0])[1]
+#     return np.array([phi], dtype=np.float32)
 
 DISCRETE_ACTION_MAP = {
     HabitatSimActions.stop: DiscreteNavigationAction.STOP,
@@ -95,22 +106,72 @@ def shortest_path_example(config):
             object_pos = ovmm_env.habitat_env.env._env.habitat_env.current_episode.candidate_objects[
                 0
             ].position
+            goal_pos = ovmm_env.habitat_env.env._env.habitat_env.current_episode.candidate_goal_receps[
+                0
+            ].position
 
+            goal_pos_viewpoints = ovmm_env.habitat_env.env._env.habitat_env.current_episode.candidate_goal_receps[
+                0
+            ].view_points
+            ious = [viewpoint.iou for viewpoint in goal_pos_viewpoints]
+            max_iou_idx = ious.index(max(ious))
+            goal_pos_max_iou_viewpoint = goal_pos_viewpoints[
+                max_iou_idx
+            ].agent_state.position
+
+            # for idx, viewpoint in enumerate(goal_pos_viewpoints):
+            #     print(f"\nViewpoint {idx}")
+            #     print(f"Viewpoint position: {viewpoint.agent_state.position}")
+            #     print(f"Viewpoint rotation: {viewpoint.agent_state.rotation}")
+            #     print(f"Viewpoint IOU: {viewpoint.iou}")
+
+            # print(f"\nMax IOU viewpoint {max_iou_idx}")
+            # print(f"Max IOU viewpoint position: {goal_pos_max_iou_viewpoint}")
+            goal = mn.Vector3(goal_pos_max_iou_viewpoint)
+            goal_orientation = quaternion_from_coeff(
+                goal_pos_viewpoints[max_iou_idx].agent_state.rotation
+            )
             while (
                 not ovmm_env.habitat_env.env._env.habitat_env.episode_over
                 and steps < max_steps
             ):
                 if steps != 0:
+                    # curr_quat = follower._sim.robot.sim_obj.rotation
+                    # curr_rotation = [
+                    #     curr_quat.vector.x,
+                    #     curr_quat.vector.y,
+                    #     curr_quat.vector.z,
+                    #     curr_quat.scalar,
+                    # ]
+                    # curr_quat = quaternion_from_coeff(
+                    # curr_rotation
+                    # )
+                    # # get heading angle
+                    # rot = _quat_to_xy_heading(
+                    #     curr_quat.inverse()
+                    # )
+                    # rot = rot - np.pi / 2
+                    # # convert back to quaternion
+                    # ang_pos = rot[0]
+                    # curr_rot = mn.Quaternion(
+                    #     mn.Vector3(0, np.sin(ang_pos / 2), 0), np.cos(ang_pos / 2)
+                    # )
+
                     f.write(
+                        f"Current agent location:\t{follower._sim.robot.base_pos}\n"
+                        # f"Current agent orientation:\t{curr_quat}\n"
+                        # f"Current agent orientation:\t{curr_rot}\n"
+                        f"Navigation goal location:\t{goal}\n"
+                        f"Navigation goal orientation:\t{goal_orientation}\n"
+                        # f"Difference between orientations:\t{curr_quat * goal_orientation.inverse()}\n"
                         f"info['ovmm_dist_to_pick_goal']:\t{info['ovmm_dist_to_pick_goal']}\n"
+                        f"info['ovmm_dist_to_keep_goal']:\t{info['ovmm_dist_to_place_goal']}\n"
                     )
 
                 f.write(f"\nTimestep: {steps}\n")
                 print(f"Timestep: {steps}")
-                best_action = DISCRETE_ACTION_MAP[
-                    follower.get_next_action(mn.Vector3(object_pos), f)
-                ]
-                f.write(f"Agent action: {best_action}\n")
+                best_action = DISCRETE_ACTION_MAP[follower.get_next_action(goal)]
+                f.write(f"Agent action taken: {best_action}\n")
                 if best_action is None:
                     break
 
