e76c61a4d24aed63560e5e7c7fa397be9ecae4dd
Chris Paxton <cpaxton@fb.com>
2023-08-29 11:40:49 -0700

    add pyg + update wget

--- install_deps.sh
+++ install_deps.sh
@@ -61,7 +61,7 @@ echo ""
 echo "Download DETIC checkpoint..."
 cd $HOME_ROBOT_ROOT/src/home_robot/home_robot/perception/detection/detic/Detic
 mkdir models
-wget https://dl.fbaipublicfiles.com/detic/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth -O models/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth
+wget --no-check-certificate https://dl.fbaipublicfiles.com/detic/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth -O models/Detic_LCOCOI21k_CLIP_SwinB_896b32_4x_ft4x_max-size.pth
 
 cd $HOME_ROBOT_ROOT
 echo ""
