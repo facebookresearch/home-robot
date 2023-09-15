a6ea490cc85a89953d0bd9ee40af9a048fb6c973
Chris Paxton <cpaxton@jhu.edu>
2023-09-15 14:49:19 -0400

    update scri

---
+++ src/home_robot_spot/examples/spot_3d_map.py
@@ -0,0 +1,90 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from typing import Dict, List
+
+from home_robot.agent.ovmm_agent import (
+    OvmmPerception,
+    build_vocab_from_category_map,
+    read_category_map_file,
+)
+from home_robot.mapping.voxel import SparseVoxelMap  # Aggregate 3d information
+from home_robot.mapping.voxel_map import (  # Sample positions in free space for our robot to move to
+    SparseVoxelMapNavigationSpace,
+)
+from home_robot.motion.spot import (  # Just saves the Spot robot footprint for kinematic planning
+    SimpleSpotKinematics,
+)
+from home_robot.utils.config import get_config, load_config
+from home_robot_spot import SpotClient
+
+
+def main():
+    spot_config = get_config("src/home_robot_spot/configs/default_config.yaml")[0]
+
+    # TODO move these parameters to config
+    voxel_size = 0.05
+    voxel_map = SparseVoxelMap(resolution=voxel_size, local_radius=0.1)
+
+    # Create kinematic model (very basic for now - just a footprint)
+    robot_model = SimpleSpotKinematics()
+
+    # Create navigation space example
+    navigation_space = SparseVoxelMapNavigationSpace(
+        voxel_map=voxel_map, robot=robot_model, step_size=0.1
+    )
+    print(" - Created navigation space and environment")
+    print(f"   {navigation_space=}")
+
+    # Create segmentation sensor and load config. Returns config from file, as well as a OvmmPerception object that can be used to label scenes.
+    print("- Loading configuration")
+    config = load_config(visualize=False)
+
+    print("- Create and load vocabulary and perception model")
+    semantic_sensor = OvmmPerception(config, 0, True, module="detic")
+    obj_name_to_id, rec_name_to_id = read_category_map_file(
+        config.ENVIRONMENT.category_map_file
+    )
+    vocab = build_vocab_from_category_map(obj_name_to_id, rec_name_to_id)
+    semantic_sensor.update_vocabulary_list(vocab, 0)
+    semantic_sensor.set_vocabulary(0)
+
+    spot = SpotClient(config=spot_config)
+    try:
+        # Turn on the robot using the client above
+        spot.start()
+
+        spot.move_base(0.0, 0.0)
+        # breakpoint()
+
+        linear = input("Input Linear: ")
+        angular = input("Input Angular: ")
+        while linear != "" and angular != "":
+            try:
+                spot.move_base(float(linear), float(angular))
+            except Exception:
+                print("Error -- try again")
+
+            obs = spot.get_rgbd_obs(semantic_sensor)
+            obs = semantic_sensor.predict(obs)
+            voxel_map.add_obs(obs, xyz_frame="world")
+            print("added, now display something")
+            voxel_map.show(backend="open3d", instances=False)
+
+            linear = input("Input Linear: ")
+            angular = input("Input Angular: ")
+            # viz_data = spot.make_3d_viz(viz_data)
+
+    except Exception as e:
+        print(e)
+        spot.stop()
+        raise e
+
+    # finally:
+    #     spot.stop()
+
+
+if __name__ == "__main__":
+    main()
