a2c24bf4fb9d4d6a5d699c88ad2bb06197af439c
jdvakil <jdvakil@gmail.com>
2023-09-18 10:38:17 -0700

    grasp

--- projects/spot/grasp.py
+++ projects/spot/grasp.py
@@ -27,18 +27,71 @@ class GraspController:
         self.top_grasp = top_grasp
         self.hor_grasp = hor_grasp
         self.detector = OwlVit(self.labels, self.confidence, self.show_img)
-        self.look = np.deg2rad(config.GAZE_ARM_JOINT_ANGLES)
-        self.stow = np.deg2rad(config.PLACE_ARM_JOINT_ANGLES)
+        self.look = np.deg2rad(config.SPOT.GAZE_ARM_JOINT_ANGLES)
+        self.stow = np.deg2rad(config.SPOT.PLACE_ARM_JOINT_ANGLES)
+        self.pick_location = []
 
     def reset_to_look(self):
+        """
+        Reset the robotic arm to a predefined 'look' position.
+
+        This method sets the joint positions of the
+        robotic arm to a predefined 'look' configuration.
+        The 'travel_time' parameter controls the speed of
+        the movement, and a delay of 1 second is added
+        to ensure stability after the movement.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         self.spot.set_arm_joint_positions(self.look, travel_time=1.0)
         time.sleep(1)
 
     def reset_to_stow(self):
+        """
+        Reset the robotic arm to a predefined 'stow' position.
+
+        This method sets the joint positions of the robotic arm
+        to a predefined 'stow' configuration.
+        The 'travel_time' parameter controls the speed of the movement,
+        and a delay of 1 second is added
+        to ensure stability after the movement.
+
+        Args:
+            None
+
+        Returns:
+            None
+        """
         self.spot.set_arm_joint_positions(self.stow, travel_time=1.0)
         time.sleep(1)
 
     def find_obj(self, img) -> np.ndarray:
+        """
+        Detect and locate an object in an image.
+
+        This method resets the robotic arm to a predefined
+        'look' position and then attempts to detect and locate
+        an object within the given image. It draws a bounding box and a
+        center point on the detected object,
+        and optionally displays the annotated image.
+
+        Args:
+            img (numpy.ndarray or list): The image in which to detect the
+            object. It can be either a numpy.ndarray
+                or a list. If it's a list, it will be converted to
+                a numpy array.
+
+        Returns:
+            np.ndarray: A numpy array representing the center coordinates of the detected object.
+
+        Raises:
+            NotImplementedError: If the object cannot be found in the image.
+            TypeError: If the input image is not of type numpy.ndarray or list.
+        """
         self.reset_to_look()
         if isinstance(img, np.ndarray) or isinstance(img, list):
             if isinstance(img, list):
@@ -70,19 +123,16 @@ class GraspController:
                     print(f" > Saved {filename}")
                 return center
             else:
-                raise NotImplementedError(f"Cannot find an object in the image")
+                return None
         else:
             raise TypeError(f"img is of type {type(img)}, expected is numpy array")
 
-    def grasp(self, image_responses, timeout=10, count=3):
-        # TODO receive label (optionally) here and update label from owlvit ()
-
-        # image_responses = spot.get_image_responses([SpotCamIds.HAND_COLOR])
-        hand_image_response = image_responses[0]  # only expecting one image
-        img = imcv2(hand_image_response)
+    def grasp(self, hand_image_response, pixels, timeout=10, count=3):
+        # hand_image_response = image_responses_local[0]  # only expecting one image
+        # img = imcv2(hand_image_response)
         k = 0
         while True:
-            pixels = self.find_obj(img=img)
+            # pixels = self.find_obj(img=img)
             if pixels is not None:
                 print(f" > Grasping object at {pixels}")
                 success = self.spot.grasp_point_in_image(
@@ -94,49 +144,62 @@ class GraspController:
                 )
                 if success:
                     print(" > Sucess")
-                    self.reset_to_stow()
-                    time.sleep(3)
-                    break
-                else:
-                    k = k + 1
-                    print(
-                        f" > Could not find object from the labels, tries left: {count - k}"
+                    self.pick_location = self.spot.get_arm_joint_positions(
+                        as_array=True
                     )
+                    self.reset_to_stow()
+                    time.sleep(1)
+                    return success
+                k = k + 1
+                print(
+                    f" > Could not find object from the labels, tries left: {count - k}"
+                )
                 if k >= count:
                     print(" > Ending trial as target trials reached")
-                    retry = input(
-                        " > Would you like to retry? y/n, or enter 'c' to enter a new label and retry: "
-                    )
-                    if retry == "y":
-                        # @JAY add a look around script and then replace with gaze
-                        k = 0
-                        continue
-                    if retry == "c":
-                        new_label = input(" > Enter new label: ")
-                        self.update_label(new_label)
-                        time.sleep(2)
-                        k = 0
-                        continue
-                    else:
-                        break
-        time.sleep(3)
+                    return success
+                    break
+            else:
+                return None
+                # new_look = self.look
+                # new_look[0] -=np.pi/4
+                # self.spot.set_arm_joint_positions(new_look, travel_time=1)
+                # pixels = self.find_obj(img=imcv2(self.spot.get_image_responses([SpotCamIds.HAND_COLOR])[0]))
+        time.sleep(1)
 
-    def update_label(self, new_label):
+    def update_label(self, new_label: str):
+        """
+        Update the labels associated with an image and configure an OwlVit detector.
+
+        This method appends a new label, formatted as "an image of {new_label}",
+        to the list of labels.
+        It also updates the OwlVit detector with the modified list of labels,
+        confidence settings, and
+        whether to display images.
+
+        Args:
+            new_label (str): Classification of the object to be detected
+
+        Returns:
+            None
+        """
         self.labels.append(f"an image of {new_label}")
         self.detector = OwlVit(self.labels, self.confidence, self.show_img)
 
+    def get_pick_location(self):
+        return self.pick_location
+
 
 if __name__ == "__main__":
-    config_path = "projects/spot/configs/config.yaml"
-    config, config_str = get_config(config_path)
+    CONFIG_PATH = "projects/spot/configs/config.yaml"
+    config, config_str = get_config(CONFIG_PATH)
     config.defrost()
     spot = Spot("RealNavEnv")
     gaze = GraspController(
         config=config,
         spot=spot,
-        objects=[["bottle of water"]],
+        objects=[["penguin plush"]],
         confidence=0.05,
-        show_img=True,
+        show_img=False,
         top_grasp=False,
         hor_grasp=True,
     )
@@ -152,4 +215,11 @@ if __name__ == "__main__":
         time.sleep(1)
         print("Resetting environment...")
         image_responses = spot.get_image_responses([SpotCamIds.HAND_COLOR])
-        gaze.grasp(image_responses=image_responses)
+        pixel = gaze.find_obj(img=imcv2(image_responses[0]))
+        gaze.grasp(hand_image_response=image_responses[0], pixels=pixel)
+        time.sleep(1)
+        pick = gaze.get_pick_location()
+        spot.set_arm_joint_positions(pick, travel_time=1)
+        time.sleep(1)
+        spot.open_gripper()
+        time.sleep(2)
