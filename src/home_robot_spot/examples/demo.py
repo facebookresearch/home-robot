a22c14a8e022e890e9a89d74b89502665d223fcf
Jay Vakil <61257499+Jdvakil@users.noreply.github.com>
2023-10-17 19:18:07 +0000

    self missing

--- src/home_robot_spot/examples/demo.py
+++ src/home_robot_spot/examples/demo.py
@@ -873,7 +873,7 @@ class SpotDemoAgent:
                         np.array([vf[0], vf[1], instance_pose[2]]), blocking=True
                     )
                 time.sleep(0.5)
-                success = self.gaze.gaze_and_grasp(finish_sweep_before_deciding=parameters['finish_grasping'])
+                success = self.gaze.gaze_and_grasp(finish_sweep_before_deciding=self.parameters['finish_grasping'])
                 time.sleep(0.5)
                 if success:
                     # TODO: @JAY make placing cleaner
