57d5282b6e3c4b9e4a2d94d467377aa6403728ad
Sasha Sax <asax@stanford.edu>
2023-10-19 14:10:39 -0700

    refactored ui to use class components

--- projects/scannet_offline_eval/demo/components/gripper_feed.py
+++ projects/scannet_offline_eval/demo/components/gripper_feed.py
@@ -6,6 +6,7 @@ import asyncio
 import base64
 import math
 from dataclasses import dataclass
+from functools import cached_property
 from typing import Dict, Optional
 
 import dash
@@ -20,10 +21,21 @@ from loguru import logger
 from pytorch3d.vis.plotly_vis import get_camera_wireframe
 
 from .app import SparseVoxelMapDirectoryWatcher, app, svm_watcher
+from .core import DashComponent
 
 
 @dataclass
-class SourceConfig:
+class VideoStreamComponentConfig:
+    image_id: str
+    trigger_id: str
+    trigger_exists: bool
+    stream_obj: SparseVoxelMapDirectoryWatcher
+    stream_attr: str
+    title: str = "Video"
+    base_css_class: str = "gripper"
+
+
+class VideoStreamComponent(DashComponent):
     # Instead of polling the server, we might prefer just establishing either
     #   - a websocket connecton (Quart: https://www.dash-extensions.com/components/websocket)
     #   - server-side events (Starlette: https://www.dash-extensions.com/components/event_source)
@@ -31,6 +43,106 @@ class SourceConfig:
     # will push updates to the client. We can tell the client what to do with the resulting data using
     # the client-side callback, as below;
     # Update div using websocket.
+    def __init__(self, name, config):
+        super().__init__(name)
+        self.config = config
+
+    def register_callbacks(self, app):
+        self.app = app
+        return self
+
+    @cached_property
+    def layout(self):
+        return dbc.Row(
+            [
+                dbc.Col(
+                    [
+                        dbc.Row(
+                            html.H2(
+                                [self.config.title],
+                                className="text-secondary text-center",
+                            ),
+                        ),
+                        html.Img(
+                            src=self.app.get_asset_url(
+                                "images/stream_paused_1_1_horiz.jpg"
+                            ),
+                            # style={"width": '95%'},  # "float": "left,
+                            id=self.config.image_id,
+                            className=f"{self.config.base_css_class}-img img-fluid",
+                        ),
+                        self.trigger_component,
+                        dcc.Store(id=f"{self.config.image_id}-count"),
+                    ],
+                    md=12,
+                    className=f"{self.config.base_css_class}-feed",
+                ),
+            ]
+        )
+
+    @cached_property
+    def trigger_component(self):
+        raise NotImplementedError
+
+
+@dataclass
+class IntervalVideoStreamComponentConfig(VideoStreamComponentConfig):
+    trigger_interval_kwargs: Optional[Dict] = None
+
+
+@dataclass
+class IntervalVideoStreamComponent(VideoStreamComponent):
+    def __init__(self, name, config):
+        super().__init__(name, config)
+
+    def register_callbacks(self, app):
+        super().register_callbacks(app)
+        app.callback(
+            [
+                Output(self.config.image_id, "src"),
+                Output(f"{self.config.image_id}-count", "data"),
+            ],
+            # [Output(self.image_id, "src")],
+            [Input(self.config.trigger_id, "n_intervals")],
+            [
+                State(f"{self.config.image_id}-count", "data"),
+            ],
+            blocking=False,
+            prevent_initial_callback=False,
+        )(self.update_gripper_feed)
+        return self
+
+    @cached_property
+    def trigger_component(self):
+        if not self.config.trigger_exists:
+            if self.config.trigger_interval_kwargs is None:
+                self.config.trigger_interval_kwargs = {}
+            trigger_element = dcc.Interval(
+                id=self.config.trigger_id,
+                **self.config.trigger_interval_kwargs,
+            )
+            return trigger_element
+
+    def update_gripper_feed(self, n_intervals, count):
+        if count == None:
+            count = -1
+        stream_obj, stream_attr = self.config.stream_obj, self.config.stream_attr
+        new_count = stream_obj.current_obs_number
+        if getattr(stream_obj, stream_attr) is None or count >= new_count:
+            raise PreventUpdate
+
+        return [
+            f"data:image/jpeg;base64, {base64.b64encode(getattr(stream_obj, stream_attr)).decode()}",
+            new_count,
+        ]
+
+
+####################
+# Old setup
+####################
+@dataclass
+class SourceConfig:
+
     image_id: str
 
     def make_html_element_and_setup_callbacks(self):
@@ -59,109 +171,6 @@ class WebsocketSourceConfig(SourceConfig):
 
 
 @dataclass
-class ClientRequestSourceConfig(SourceConfig):
-    """
-    Connects to an existing webserver at
-    """
-
-    app: DashProxy
-    trigger_id: str
-    svm_watcher: SparseVoxelMapDirectoryWatcher
-    trigger_exists: bool
-    trigger_interval_kwargs: Optional[Dict] = None
-    svm_watcher_attr: str = "obstacle"
-
-    def make_html_element_and_setup_callbacks_with_cam_coords(self):
-        @self.app.callback(
-            [
-                Output(self.image_id, "src"),
-                Output("realtime-3d-camera-coords", "data"),
-                Output(f"{self.image_id}-count", "data"),
-            ],
-            # [Output(self.image_id, "src")],
-            [Input(self.trigger_id, "n_intervals")],
-            [
-                State("realtime-3d-fig-names", "data"),
-                State(f"{self.image_id}-count", "data"),
-            ],
-            #   [Input("gripper-feed-interval", "n_intervals")],
-            blocking=False,
-            prevent_initial_callback=False,
-        )
-        def update_gripper_feed(n_intervals, trace_names, count):
-            if count == None:
-                count = -1
-            new_count = svm_watcher.current_obs_number
-            if (
-                svm_watcher.rgb_jpeg is None
-                or trace_names is None
-                or count >= new_count
-            ):
-                raise PreventUpdate
-
-            # logger.debug(f"Updating gripper feed image {svm_watcher.rgb_jpeg.shape}")
-            return [
-                f"data:image/jpeg;base64, {base64.b64encode(svm_watcher.rgb_jpeg).decode()}",
-                svm_watcher.cam_coords,
-                new_count,
-            ]
-            # return f"data:image/jpeg;base64, {base64.b64encode(svm_watcher.rgb_jpeg).decode()}"
-
-        if not self.trigger_exists:
-            if self.trigger_interval_kwargs is None:
-                self.trigger_interval_kwargs = {}
-            trigger_element = dcc.Interval(
-                id=self.trigger_id,
-                **self.trigger_interval_kwargs,
-                # interval=int(update_frequency_ms),  # in milliseconds,
-                # disabled=True,
-            )
-            return trigger_element
-
-    def make_html_element_and_setup_callbacks(self):
-        @self.app.callback(
-            [
-                Output(self.image_id, "src"),
-                Output(f"{self.image_id}-count", "data"),
-            ],
-            # [Output(self.image_id, "src")],
-            [Input(self.trigger_id, "n_intervals")],
-            [
-                State("realtime-3d-fig-names", "data"),
-                State(f"{self.image_id}-count", "data"),
-            ],
-            blocking=False,
-            prevent_initial_callback=False,
-        )
-        def update_gripper_feed(n_intervals, trace_names, count):
-            if count == None:
-                count = -1
-            new_count = svm_watcher.current_obs_number
-            if (
-                getattr(svm_watcher, self.svm_watcher_attr) is None
-                or trace_names is None
-                or count >= new_count
-            ):
-                raise PreventUpdate
-
-            return [
-                f"data:image/jpeg;base64, {base64.b64encode(getattr(svm_watcher, self.svm_watcher_attr)).decode()}",
-                new_count,
-            ]
-
-        if not self.trigger_exists:
-            if self.trigger_interval_kwargs is None:
-                self.trigger_interval_kwargs = {}
-            trigger_element = dcc.Interval(
-                id=self.trigger_id,
-                **self.trigger_interval_kwargs,
-                # interval=int(update_frequency_ms),  # in milliseconds,
-                # disabled=True,
-            )
-            return trigger_element
-
-
-@dataclass
 class ServerSideEventSourceConfig(SourceConfig):
     app: DashProxy
     event_url: str
@@ -169,37 +178,6 @@ class ServerSideEventSourceConfig(SourceConfig):
 
     def make_html_element_and_setup_callbacks(self, app, image_id, trigger_id):
         raise NotImplementedError
-
-
-def make_feed_with_cam_coord_callback(
-    source_cfg, name="Live Gripper Feed", base_css_class="gripper"
-):
-    trigger_element = source_cfg.make_html_element_and_setup_callbacks_with_cam_coords()
-
-    return dbc.Row(
-        [
-            dbc.Col(
-                [
-                    dbc.Row(
-                        html.H2(
-                            [name],
-                            className="text-secondary text-center",
-                        ),
-                    ),
-                    html.Img(
-                        src=app.get_asset_url("images/stream_paused_1_1_horiz.jpg"),
-                        # style={"width": '95%'},  # "float": "left,
-                        id=source_cfg.image_id,
-                        className=f"{base_css_class}-img img-fluid",
-                    ),
-                    trigger_element,
-                    dcc.Store(id=f"{source_cfg.image_id}-count"),
-                ],
-                md=12,
-                className=f"{base_css_class}-feed",
-            ),
-        ]
-    )
 
 
 def make_feed(source_cfg, name="Live Gripper Feed", base_css_class="gripper"):
--- projects/scannet_offline_eval/demo/server.py
+++ projects/scannet_offline_eval/demo/server.py
@@ -11,19 +11,17 @@ from textwrap import dedent
 
 import dash
 import dash_bootstrap_components as dbc
-import openai
 from components.app import app, app_config, svm_watcher
-
-# from components.chat_old import make_layout as make_chat_layout
 from components.chat import Chatbox, ChatBoxConfig
-from components.gripper_feed import (
-    ClientRequestSourceConfig,
-    WebsocketSourceConfig,
-    make_feed,
-    make_feed_with_cam_coord_callback,
+from components.demo_header import DemoHeader  # make_header_layout
+from components.gripper_feed import (  # ClientRequestSourceConfig,; WebsocketSourceConfig,; make_feed,; make_feed_with_cam_coord_callback,
+    IntervalVideoStreamComponent,
+    IntervalVideoStreamComponentConfig,
 )
-from components.index_layout import make_header_layout
-from components.realtime_3d import make_layout as make_realtime_3d_layout
+from components.realtime_3d import (  # make_layout as make_realtime_3d_layout
+    Realtime3dComponent,
+    Realtime3dComponentConfig,
+)
 from dash import Patch, dcc, html
 from dash.dependencies import Input, Output, State
 from loguru import logger
@@ -32,109 +30,99 @@ from loguru import logger
 #     "LOGURU_FORMAT"
 # ] = "| <level>{level: <8}</level> |<cyan>{name:^45}</cyan>|<level>{function:^22}</level>| <cyan>{line:<3} |</cyan> - <level>{message}</level> <lg>@ [{time:YYYY-MM-DD HH:mm:ss.SSS}]</lg> "
 
-figure = svm_watcher.svm.show(
-    backend="pytorch3d",
-    instances=False,
-    mock_plot=True,
-    pointcloud_marker_size=2,  # int(app_config.pointcloud_voxel_size * 100)
-)
-camera = dict(eye=dict(x=0.0, y=0.0, z=app_config.camera_initial_distance))
-figure.update_layout(
-    autosize=True,
-    scene_camera=camera,
-    margin=dict(
-        l=20,
-        r=20,
-        b=20,
-        t=20,
+##################
+# Make components
+##################
+# Header
+header = DemoHeader(name="demo-header")
+
+# RGB
+rgb_config = IntervalVideoStreamComponentConfig(
+    title="Egocentric Camera [RGBD]",
+    image_id="rgb-img",
+    trigger_id="gripper-feed-interval",
+    trigger_exists=False,
+    stream_obj=svm_watcher,
+    stream_attr="rgb_jpeg",
+    base_css_class="ego",
+    trigger_interval_kwargs=dict(
+        interval=int(app_config.video_feed_update_freq_ms),
+        disabled=False,
     ),
 )
+rgb_component = IntervalVideoStreamComponent(name="rgb-img", config=rgb_config)
 
+# Depth
+depth_config = IntervalVideoStreamComponentConfig(
+    title="",
+    image_id="depth-img",
+    trigger_id="gripper-feed-interval",
+    trigger_exists=True,
+    stream_obj=svm_watcher,
+    stream_attr="depth_jpeg",
+    base_css_class="ego",
+)
+depth_component = IntervalVideoStreamComponent(name="depth-img", config=depth_config)
+
+# Instance Map figure
+r3d_config = Realtime3dComponentConfig(
+    figure=svm_watcher.svm.show(
+        backend="pytorch3d",
+        instances=False,
+        mock_plot=True,
+        pointcloud_marker_size=2,  # int(app_config.pointcloud_voxel_size * 100)
+    ),
+    update_on_start=not app_config.start_paused,
+    update_frequency_ms=app_config.pointcloud_update_freq_ms,
+    update_camera_coord_frequence_ms=app_config.video_feed_update_freq_ms,
+)
+r3d_component = Realtime3dComponent(name="realtime-3d", config=r3d_config)
+
+# Map
+map_config = IntervalVideoStreamComponentConfig(
+    title="Birds-Eye Obstacle Map",
+    image_id="map-img",
+    trigger_id="gripper-feed-interval",
+    trigger_exists=True,
+    stream_obj=svm_watcher,
+    stream_attr="map_im",
+    base_css_class="map",
+)
+map_component = IntervalVideoStreamComponent(name="map-img", config=map_config)
+
+# Chatbox
 chatbox_config = ChatBoxConfig(
     chat_log_fpath=Path(os.path.dirname(app_config.directory_watch_path))
     / "demo_chat.json"
 )
 chatbox = Chatbox(chatbox_config, name="chatbox")
 
+######################
+
+
+##############
+# App layout
+##############
 # app.config.suppress_callback_exceptions = True
 app.layout = dbc.Container(
     children=[
-        make_header_layout(),
+        header.register_callbacks(app).layout,
         dbc.Row(
             [
                 dbc.Col(
                     [
-                        make_feed_with_cam_coord_callback(
-                            # # Run publisher_server.py to set up server
-                            # WebsocketSourceConfig(
-                            #     app=app,
-                            #     image_id="gripper-feed-img",
-                            #     trigger_id="gripper-feed-ws",
-                            #     websocket_url="ws://127.0.0.1:5000/gripper-feed-ws",
-                            # ),
-                            ClientRequestSourceConfig(
-                                app=app,
-                                image_id="gripper-feed-img",
-                                trigger_id="gripper-feed-interval",  # realtime-3d
-                                trigger_exists=False,
-                                svm_watcher=svm_watcher,
-                                trigger_interval_kwargs=dict(
-                                    interval=int(app_config.video_feed_update_freq_ms),
-                                    disabled=False,
-                                ),
-                            ),
-                            name="Egocentric Camera [RGBD]",
-                            base_css_class="ego",
-                            # name="Live 2D Map",
-                        ),
-                        make_feed(
-                            ClientRequestSourceConfig(
-                                app=app,
-                                image_id="depth-feed-img",
-                                trigger_id="gripper-feed-interval",  # realtime-3d
-                                trigger_exists=True,
-                                svm_watcher=svm_watcher,
-                                svm_watcher_attr="depth_jpeg",
-                                # trigger_interval_kwargs=dict(
-                                #     interval=int(app_config.video_feed_update_freq_ms) * 2,
-                                #     disabled=False,
-                                # ),
-                            ),
-                            name="",
-                            base_css_class="ego",
-                        ),
+                        rgb_component.register_callbacks(app).layout,
+                        depth_component.register_callbacks(app).layout,
                     ],
                     width=2,
                 ),
                 dbc.Col(
-                    [
-                        make_realtime_3d_layout(
-                            figure,
-                            app_config.pointcloud_update_freq_ms,
-                            disabled=app_config.start_paused,
-                        )
-                    ],
+                    [r3d_component.register_callbacks(app).layout],
                     width=7,
                 ),
                 dbc.Col(
                     children=[
-                        make_feed(
-                            ClientRequestSourceConfig(
-                                app=app,
-                                image_id="map-2d-img",
-                                trigger_id="realtime-3d-interval",  # realtime-3d
-                                trigger_exists=True,
-                                svm_watcher=svm_watcher,
-                                svm_watcher_attr="map_im",
-                                # trigger_interval_kwargs=dict(
-                                #     interval=int(app_config.video_feed_update_freq_ms) * 2,
-                                #     disabled=False,
-                                # ),
-                            ),
-                            name="Birds-Eye Obstacle Map",
-                            base_css_class="map",
-                        ),
-                        # make_chat_layout(),
+                        map_component.register_callbacks(app).layout,
                         chatbox.register_callbacks(app).layout,
                     ],
                     width=3,
@@ -146,6 +134,8 @@ app.layout = dbc.Container(
     fluid=True,
     className="h-100",
 )
+##############
+
 
 if __name__ == "__main__":
     import argparse
