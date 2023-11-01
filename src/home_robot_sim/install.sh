a105bc817ddea6ab11c0c34e790b62699cafde60
Chris Paxton <cpaxton@jhu.edu>
2023-11-01 14:16:49 -0400

    fixing installation issues for simulation + make sure rendering test works

---
+++ src/home_robot_sim/install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+echo ""
+echo ""
+echo "=============================================="
+echo "           INSTALLING SIMULATION"
+echo "=============================================="
+echo "Make sure:"
+echo " - Conda env is active"
+echo " - CUDA_HOME is set"
+echo " - HOME_ROBOT_ROOT is set"
+echo "Currently:"
+echo " - CUDA_HOME=$CUDA_HOME"
+echo " - HOME_ROBOT_ROOT=$HOME_ROBOT_ROOT"
+echo " - python=`which python`"
+echo ""
+read -p "Does all this look correct? (y/n) " yn
+case $yn in
+	y ) echo "Starting installation...";;
+	n ) echo "Exiting...";
+		exit;;
+	* ) echo Invalid response!;
+		exit 1;;
+esac
+
+conda env update -f $HOME_ROBOT_ROOT/src/home_robot_sim/environment.yml
+cd $HOME_ROBOT_ROOT
+pip install -e $HOME_ROBOT_ROOT/src/home_robot_sim
+
+echo ""
+echo "Install habitat dependencies..."
+cd $HOME_ROBOT_ROOT
+git submodule update --init --recursive src/third_party/habitat-lab
+pip install -e src/third_party/habitat-lab/habitat-lab
+pip install -e src/third_party/habitat-lab/habitat-baselines
+
+echo ""
+echo "Download data from submodules..."
+cd $HOME_ROBOT_ROOT
+git submodule update --init --recursive 
