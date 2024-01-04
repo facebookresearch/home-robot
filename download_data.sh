2b74b4aa713aac7be2d585556e8c8b24e95a29f3
Chris Paxton <cpaxton@fb.com>
2024-01-04 16:46:13 -0500

    update scripts to make sure we dont accidentally download all this data

--- download_data.sh
+++ download_data.sh
@@ -4,24 +4,43 @@
 # This source code is licensed under the MIT license found in the
 # LICENSE file in the root directory of this source tree.
 
+
+# check command-line arguments
+yes=false
+for arg in "$@"; do
+  case "$arg" in
+    --yes|-y)
+      yes=true
+      shift
+      ;;
+    *)
+      # handle other arguments here, if needed
+      ;;
+  esac
+done
+
+
 echo ""
 echo ""
 echo "=============================================="
 echo "            DOWNLOADING SIM DATA"
 echo "=============================================="
-echo "make sure:"
-echo " - home_robot_root is set"
-echo "currently:"
-echo " - home_robot_root=$home_robot_root"
-echo ""
-read -p "does all this look correct? (y/n) " yn
-case $yn in
-	y ) echo "starting installation...";;
-	n ) echo "exiting...";
-		exit;;
-	* ) echo invalid response!;
-		exit 1;;
-esac
+echo "Make sure:"
+echo " - HOME_ROBOT_ROOT is set"
+echo "Currently:"
+echo " - HOME_ROBOT_ROOT=$HOME_ROBOT_ROOT"
+
+if [ "$yes" = false ]; then
+	echo ""
+	read -p "Does all this look correct? (y/n) " yn
+	case $yn in
+		y ) echo "starting installation...";;
+		n ) echo "exiting...";
+			exit;;
+		* ) echo invalid response!;
+			exit 1;;
+	esac
+fi
 
 cd $HOME_ROBOT_ROOT
 
@@ -34,4 +53,13 @@ git clone https://huggingface.co/datasets/ai-habitat/OVMM_objects objects --recu
 
 cd $HOME_ROBOT_ROOT/data/datasets
 git clone https://huggingface.co/datasets/ai-habitat/OVMM_episodes ovmm --recursive
+
+echo ""
+echo "Download the robot model Habitat uses..."
+mkdir -p $HOME_ROBOT_ROOT/data/robots/hab_stretch
+cd $HOME_ROBOT_ROOT/data/robots/hab_stretch
+wget --no-check-certificate http://dl.fbaipublicfiles.com/habitat/robots/hab_stretch_v1.0.zip
+unzip -o hab_stretch_v1.0.zip
+cd $HOME_ROBOT_ROOT
+
 
