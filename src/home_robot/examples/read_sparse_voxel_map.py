022d3cf45b14e0a6a808816b66377ae39b9987bc
Chris Paxton <cpaxton@fb.com>
2024-02-07 12:12:42 -0500

    visualize positions

--- src/home_robot/examples/read_sparse_voxel_map.py
+++ src/home_robot/examples/read_sparse_voxel_map.py
@@ -83,11 +83,17 @@ def main(
         agent = None
         voxel_map = SparseVoxelMap(resolution=voxel_size)
 
+    # TODO: read this from file or something
+    # x0 = np.array([0, 0, 0])
+    # x0 = np.array([0.0, -0.0, 1.5707968])
+    x0 = np.array([1.1499997, -0.60000074, -1.4168407])
+    start_xy = [x0[0], x0[1], 0]
+
     if agent is not None:
         print("Agent loaded:", agent)
         # Display with agent overlay
         if show_svm:
-            voxel_map.show(instances=True)
+            voxel_map.show(instances=True, orig=start_xy)
         obstacles, explored = voxel_map.get_2d_map(debug=False)
         space = agent.get_navigation_space()
         # TODO: read the parameter from the agent
@@ -116,8 +122,6 @@ def main(
         plt.show()
 
         print("--- Sampling goals ---")
-        # x0 = np.array([0, 0, 0])
-        x0 = np.array([0.0, -0.0, 1.5707968])
         start_is_valid = space.is_valid(x0, verbose=True, debug=True)
         if not start_is_valid:
             print("you need to manually set the start pose to be valid")
